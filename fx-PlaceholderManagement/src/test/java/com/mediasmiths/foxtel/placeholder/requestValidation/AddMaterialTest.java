704830febb363b400d4d3bbaaed635e415923025
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-09-07 16:30:01 +0100

    AddMaterial test

--- fx-PlaceholderManagement/src/test/java/com/mediasmiths/foxtel/placeholder/requestValidation/AddMaterialTest.java
+++ fx-PlaceholderManagement/src/test/java/com/mediasmiths/foxtel/placeholder/requestValidation/AddMaterialTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.GregorianCalendar;
 
 import javax.xml.bind.JAXBException;
@@ -25,6 +26,8 @@ import au.com.foxtel.cf.mam.pms.Source;
 
 import com.mediasmiths.foxtel.placeholder.PlaceHolderMessageValidationResult;
 import com.mediasmiths.foxtel.placeholder.messagecreation.FileWriter;
+import com.mediasmiths.mayam.MayamClientErrorCode;
+import com.mediasmiths.mayam.MayamClientException;
 
 public class AddMaterialTest extends PlaceHolderMessageValidatorTest {
 
@@ -38,18 +41,14 @@ public class AddMaterialTest extends PlaceHolderMessageValidatorTest {
 	private final static String MESSAGE_ID = "123456asdfg";
 	private final static String SENDER_ID = "123456asdfg";
 	
-	private final static GregorianCalendar ORDER_CREATED = new GregorianCalendar(2000, 1, 1, 0, 0, 1);
-	private final static GregorianCalendar REQUIRED_BY = new GregorianCalendar(2000, 1, 10, 0, 0, 1);
+	private final static GregorianCalendar JAN1st = new GregorianCalendar(2000, 1, 1, 0, 0, 1);
+	private final static GregorianCalendar JAN10th = new GregorianCalendar(2000, 1, 10, 0, 0, 1);
 	
 	@Test
 	public void testValidAddMaterial() throws Exception{
 
-		PlaceholderMessage pm = buildAddMaterialRequest(EXISTING_TITLE);
-		
-		//marshall and write to file
-		FileWriter writer = new FileWriter();
-		File temp = File.createTempFile("addMaterialTest", ".xml");		
-		writer.writeObjectToFile(pm, temp.getAbsolutePath());
+		PlaceholderMessage pm = buildAddMaterialRequest(EXISTING_TITLE);		
+		File temp = createTempXMLFile(pm,"validAddMaterial");
 		
 		//prepare mock mayamClient
 		when(mayamClient.titleExists(EXISTING_TITLE)).thenReturn(true);
@@ -57,65 +56,79 @@ public class AddMaterialTest extends PlaceHolderMessageValidatorTest {
 		//test that the generated placeholder message is valid
 		assertEquals(PlaceHolderMessageValidationResult.IS_VALID,toTest.validateFile(temp.getAbsolutePath()));
 	}
+
 	
 	@Test
 	public void testAddMaterialTitleDoesntExist() throws Exception{
 		PlaceholderMessage pm = buildAddMaterialRequest(NOT_EXISTING_TITLE);
-		
-		//marshall and write to file
-		FileWriter writer = new FileWriter();
-		File temp = File.createTempFile("addMaterialTest", ".xml");		
-		writer.writeObjectToFile(pm, temp.getAbsolutePath());
+		File temp = createTempXMLFile(pm,"addMaterialTitleDoesntExist");
 		
 		//prepare mock mayamClient
 		when(mayamClient.titleExists(NOT_EXISTING_TITLE)).thenReturn(false);
 		
-		//test that the generated placeholder message is valid
+		//test that the generated placeholder message fails validation for the correct reason
 		assertEquals(PlaceHolderMessageValidationResult.NO_EXISTING_TITLE_FOR_MATERIAL,toTest.validateFile(temp.getAbsolutePath()));
 	}
 	
 	@Test
-	public void testAddMaterialOrderCreatedAfterRequiredBy(){
-		assertTrue(false);
+	public void testAddMaterialOrderCreatedAfterRequiredBy() throws IOException, Exception{
+		PlaceholderMessage pm = buildAddMaterialRequest(EXISTING_TITLE, JAN10th,JAN1st);
+		File temp = createTempXMLFile(pm,"addMaterialTitleDoesntExist");
+		//prepare mock mayamClient
+		when(mayamClient.titleExists(EXISTING_TITLE)).thenReturn(true);
+		//test that the generated placeholder message fails validation for the correct reason
+		assertEquals(PlaceHolderMessageValidationResult.ORDER_CREATED_AND_REQUIREDBY_DATES_NOT_IN_ORDER,toTest.validateFile(temp.getAbsolutePath()));
 	}
-	
-	@Test
-	public void testAddMaterialTitleExistRequestFails() throws Exception{
 		
+	
+	@Test(expected=MayamClientException.class)
+	public void testAddMaterialTitleExistRequestFails() throws Exception{
+		PlaceholderMessage pm = buildAddMaterialRequest(EXISTING_TITLE);
+		File temp = createTempXMLFile(pm,"addMaterialTitleExistRequestFails");
+		
+		//prepare mock mayamClient
+		when(mayamClient.titleExists(EXISTING_TITLE)).thenThrow(new MayamClientException(MayamClientErrorCode.FAILURE));
+				
+		//try to call validation, expect an exception
+		toTest.validateFile(temp.getAbsolutePath());
+	}
+
+	private PlaceholderMessage buildAddMaterialRequest(String titleID, GregorianCalendar created, GregorianCalendar required) throws DatatypeConfigurationException{
+				//build request
+				XMLGregorianCalendar orderCreated = DatatypeFactory.newInstance().newXMLGregorianCalendar(created);
+				XMLGregorianCalendar requiredBy =  DatatypeFactory.newInstance().newXMLGregorianCalendar(required);
+
+				Order order = new Order();
+				order.setOrderCreated(orderCreated);
+				
+				Aggregation aggregation = new Aggregation();
+				aggregation.setOrder(order);
+				
+				Source s = new Source();
+				s.setAggregation(aggregation);
+				
+				MaterialType m = new MaterialType();
+				m.setMaterialD(NEW_MATERIAL_ID);
+				m.setSource(s);
+				m.setRequiredBy(requiredBy);
+
+				AddOrUpdateMaterial aum = new AddOrUpdateMaterial();
+				aum.setTitleID(titleID);
+				aum.setMaterial(m);
+				
+				Actions actions = new Actions();
+				actions.getCreateOrUpdateTitleOrPurgeTitleOrAddOrUpdateMaterial().add(aum);
+				
+				PlaceholderMessage pm = new PlaceholderMessage();
+				pm.setMessageID(MESSAGE_ID);
+				pm.setSenderID(SENDER_ID);
+				pm.setActions(actions);
+				return pm;
 	}
 
 	private PlaceholderMessage buildAddMaterialRequest(String titleID)
 			throws DatatypeConfigurationException {
-		//build request
-		XMLGregorianCalendar orderCreated = DatatypeFactory.newInstance().newXMLGregorianCalendar(ORDER_CREATED);
-		XMLGregorianCalendar requiredBy =  DatatypeFactory.newInstance().newXMLGregorianCalendar(REQUIRED_BY);
-
-		Order order = new Order();
-		order.setOrderCreated(orderCreated);
-		
-		Aggregation aggregation = new Aggregation();
-		aggregation.setOrder(order);
-		
-		Source s = new Source();
-		s.setAggregation(aggregation);
-		
-		MaterialType m = new MaterialType();
-		m.setMaterialD(NEW_MATERIAL_ID);
-		m.setSource(s);
-		m.setRequiredBy(requiredBy);
-
-		AddOrUpdateMaterial aum = new AddOrUpdateMaterial();
-		aum.setTitleID(titleID);
-		aum.setMaterial(m);
-		
-		Actions actions = new Actions();
-		actions.getCreateOrUpdateTitleOrPurgeTitleOrAddOrUpdateMaterial().add(aum);
-		
-		PlaceholderMessage pm = new PlaceholderMessage();
-		pm.setMessageID(MESSAGE_ID);
-		pm.setSenderID(SENDER_ID);
-		pm.setActions(actions);
-		return pm;
+			return buildAddMaterialRequest(titleID,JAN1st,JAN10th);
 	}
 	
 	
--- fx-PlaceholderManagement/src/test/java/com/mediasmiths/foxtel/placeholder/requestValidation/PlaceHolderMessageValidatorTest.java
+++ fx-PlaceholderManagement/src/test/java/com/mediasmiths/foxtel/placeholder/requestValidation/PlaceHolderMessageValidatorTest.java
@@ -2,13 +2,19 @@ package com.mediasmiths.foxtel.placeholder.requestValidation;
 
 import static org.mockito.Mockito.mock;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import org.xml.sax.SAXException;
 
+import au.com.foxtel.cf.mam.pms.PlaceholderMessage;
+
 import com.mediasmiths.foxtel.placeholder.PlaceHolderMessageValidator;
+import com.mediasmiths.foxtel.placeholder.messagecreation.FileWriter;
 import com.mediasmiths.mayam.MayamClient;
 
 public abstract class PlaceHolderMessageValidatorTest {
@@ -24,4 +30,14 @@ public abstract class PlaceHolderMessageValidatorTest {
 
 	}
 
+
+	protected File createTempXMLFile(PlaceholderMessage pm, String description) throws IOException,
+			Exception {
+		//marshall and write to file
+		FileWriter writer = new FileWriter();
+		File temp = File.createTempFile(description, ".xml");		
+		writer.writeObjectToFile(pm, temp.getAbsolutePath());
+		return temp;
+	}
+	
 }
