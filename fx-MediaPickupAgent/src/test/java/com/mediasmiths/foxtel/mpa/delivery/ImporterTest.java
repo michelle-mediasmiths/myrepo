1199b8d2d54692432efdaf95b89f1086a3633426
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-09-19 16:50:39 +0100

    add importer test

---
+++ fx-MediaPickupAgent/src/test/java/com/mediasmiths/foxtel/mpa/delivery/ImporterTest.java
@@ -0,0 +1,22 @@
+package com.mediasmiths.foxtel.mpa.delivery;
+
+public class ImporterTest {
+
+	public void testDelivery(){
+		
+		//TODO : test delivery of files to the ardome auto import folder
+		
+	}
+	
+	public void testDeliveryRetrying(){
+		
+		//TODO : test that if delivery fails it will be retried a configurable number of times
+		
+	}
+	
+	public void testDeliveryFailure(){
+		
+		//TODO : test file moves to failure folder and notifications sent
+	}
+	
+}
