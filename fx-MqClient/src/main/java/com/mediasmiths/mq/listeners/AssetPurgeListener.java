31ace87cc17d737b7f41256d28606bd07629a9da
David Gault <david.gault@mediasmiths.com>
2012-10-05 16:58:52 +0100

    New MqClient project

---
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/AssetPurgeListener.java
@@ -0,0 +1,26 @@
+package com.mediasmiths.mq.listeners;
+
+import com.mayam.wf.mq.MqMessage;
+import com.mayam.wf.mq.Mq.Listener;
+import com.mayam.wf.mq.common.ContentTypes;
+import com.mayam.wf.ws.client.TasksClient;
+import com.mediasmiths.mayam.controllers.MayamTaskController;
+
+public class AssetPurgeListener 
+{
+	public static Listener getInstance(final TasksClient client, final MayamTaskController taskController) 
+	{
+		return new Listener() 
+		{
+			public void onMessage(MqMessage msg) throws Throwable 
+			{
+				if (msg.getType().equals(ContentTypes.ATTRIBUTES)) 
+				{
+					//TODO: IMPLEMENT
+					// - Purge of temporary assets notification received, remove from other worklist
+					// - How to tell if an asset is ready to be purged? Check the Expiry date?
+				}
+			}
+		};
+	}
+}
