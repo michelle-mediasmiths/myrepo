09c3fa8d7b742fb72912f8701a65302bb36aedbe
David Gault <david.gault@mediasmiths.com>
2012-10-10 12:12:24 +0100

    Package Update Listener Implementation

--- fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/PackageUpdateListener.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/PackageUpdateListener.java
@@ -1,9 +1,19 @@
 package com.mediasmiths.mq.listeners;
 
+import java.util.List;
+
+import com.mayam.wf.attributes.shared.Attribute;
+import com.mayam.wf.attributes.shared.AttributeMap;
+import com.mayam.wf.attributes.shared.type.FilterCriteria;
+import com.mayam.wf.attributes.shared.type.TaskState;
 import com.mayam.wf.mq.MqMessage;
 import com.mayam.wf.mq.Mq.Listener;
 import com.mayam.wf.mq.common.ContentTypes;
 import com.mayam.wf.ws.client.TasksClient;
+import com.mayam.wf.ws.client.TasksClient.FilterResult;
+import com.mayam.wf.ws.client.jaxws.AssetType;
+import com.mediasmiths.mayam.MayamAssetType;
+import com.mediasmiths.mayam.MayamTaskListType;
 import com.mediasmiths.mayam.controllers.MayamTaskController;
 
 public class PackageUpdateListener 
@@ -16,9 +26,129 @@ public class PackageUpdateListener
 			{
 				if (msg.getType().equals(ContentTypes.ATTRIBUTES)) 
 				{
-					//TODO: IMPLEMENT
-					//After a TX-package has been updated in Viz Ardome, the WFE shall determine if the TX- package should be added to the Segmentation Worklist
-				
+					AttributeMap messageAttributes = msg.getSubject();
+					String assetID = messageAttributes.getAttribute(Attribute.ASSET_ID);
+					String assetType = messageAttributes.getAttribute(Attribute.ASSET_TYPE);
+					
+					//TODO: Need to confirm check for already existing asset
+					if (assetID == null || assetID.equals(""))
+					{			
+						// If the message is for an existing package
+						if (assetType.equals(AssetType.PACK))
+						{
+							//Update metadata for asset
+							client.updateAsset(messageAttributes);
+							
+							AttributeMap filterEqualities = client.createAttributeMap();
+							filterEqualities.setAttribute(Attribute.TASK_LIST_ID, MayamTaskListType.SEGMENTATION.toString());
+							filterEqualities.setAttribute(Attribute.ASSET_ID, assetID);
+							FilterCriteria criteria = new FilterCriteria();
+							criteria.setFilterEqualities(filterEqualities);
+							FilterResult existingTasks = client.getTasks(criteria, 10, 0);
+							
+							// Check that no segmentation task already exists
+							if (existingTasks.getTotalMatches() == 0) {
+								boolean requiresSegTask = false;
+								
+								//Retrieve the tx-tasks and preview tasks associated with the asset
+								filterEqualities.clear();
+								filterEqualities.setAttribute(Attribute.TASK_LIST_ID, MayamTaskListType.TX_DELIVERY.toString());
+								filterEqualities.setAttribute(Attribute.ASSET_ID, assetID);
+								criteria.setFilterEqualities(filterEqualities);
+								FilterResult txTasks = client.getTasks(criteria, 10, 0);
+								boolean txReady = false;
+								
+								filterEqualities.clear();
+								filterEqualities.setAttribute(Attribute.TASK_LIST_ID, MayamTaskListType.PREVIEW.toString());
+								filterEqualities.setAttribute(Attribute.ASSET_ID, assetID);
+								criteria.setFilterEqualities(filterEqualities);
+								FilterResult previewTasks = client.getTasks(criteria, 10, 0);
+								
+								if (txTasks.getTotalMatches() > 0) {
+									//Check that the status is set to ready
+									List<AttributeMap> txTaskList = txTasks.getMatches();
+									for (int i = 0; i < txTasks.getTotalMatches(); i++) {
+										AttributeMap txTask = txTaskList.get(i);
+										TaskState taskState = txTask.getAttribute(Attribute.TASK_STATE);
+										
+										//TODO: Confirm that a task state of Open (rather than Active) equates to tx Ready
+										if (taskState.equals(TaskState.OPEN)) {
+											txReady = true;
+										}
+									}
+								}
+								
+								if (txReady && previewTasks.getTotalMatches() > 0) {
+									//Check that the status is set to pass
+									List<AttributeMap> previewTaskList = previewTasks.getMatches();
+									for (int i = 0; i < previewTasks.getTotalMatches(); i++) {
+										AttributeMap previewTask = previewTaskList.get(i);
+										TaskState taskState = previewTask.getAttribute(Attribute.TASK_STATE);
+										
+										//TODO: Confirm that a task state of Finished equates to preview passed
+										if (taskState.equals(TaskState.FINISHED)) {
+											requiresSegTask = true;
+										}
+									}
+								}
+								
+								if (requiresSegTask) {
+									//Create new segmentation task for this asset
+									long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.SEGMENTATION);
+									AttributeMap newTask = client.getTask(taskID);
+									newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
+									client.updateTask(newTask);
+								}
+							}
+						}
+					}
+					else {
+						// If the message is for a package which doesnt exist
+						if (assetType.equals(AssetType.PACK))
+						{	
+							//Create new Tx-Package + associate with parent (setting Parent ID should be handled automatically)
+							client.createAsset(messageAttributes);
+							
+							long parentId = 0;
+							//TODO: Parent ID attribute not yet implemented
+							//parentId = messageAttributes.getAttribute(Attribute.ASSET_PARENT_ID);
+							
+							
+							//TODO: set the ACLs for the Item in accordance with the FOXTEL security model
+							
+							
+							//If parent asset has a Preview status of Pass then create a new segmentation task
+							boolean requiresSegTask = false;
+							AttributeMap filterEqualities = client.createAttributeMap();
+							filterEqualities.setAttribute(Attribute.ASSET_TYPE, MayamTaskListType.PREVIEW.toString());
+							filterEqualities.setAttribute(Attribute.ASSET_ID, parentId);
+							FilterCriteria criteria = new FilterCriteria();
+							criteria.setFilterEqualities(filterEqualities);
+							FilterResult previewTasks = client.getTasks(criteria, 10, 0);
+							
+							if (previewTasks.getTotalMatches() > 0) {
+								//Check that the status is set to pass
+								List<AttributeMap> previewTaskList = previewTasks.getMatches();
+								for (int i = 0; i < previewTasks.getTotalMatches(); i++) {
+									AttributeMap previewTask = previewTaskList.get(i);
+									TaskState taskState = previewTask.getAttribute(Attribute.TASK_STATE);
+									
+									//TODO: Confirm that a task state of Finished equates to preview passed
+									if (taskState.equals(TaskState.FINISHED)) {
+										requiresSegTask = true;
+									}
+								}
+							}
+							
+							if (requiresSegTask) {
+								//Create new segmentation task for this asset
+								long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.SEGMENTATION);
+								AttributeMap newTask = client.getTask(taskID);
+								newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
+								client.updateTask(newTask);
+							}
+						}
+					}
 				}
 			}
 		};
