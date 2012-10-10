ac0166ff8ec57d2b7ea8611dd7ea8d7763dc1e2a
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-10-10 11:39:58 +0100

    stop maven trying to build mayamadapter with java 1.3

--- fx-MayamAdapter/src/main/java/com/mediasmiths/foxtel/mayam/adapter/service/MayamAdapterRestService.java
+++ fx-MayamAdapter/src/main/java/com/mediasmiths/foxtel/mayam/adapter/service/MayamAdapterRestService.java
@@ -1,8 +1,11 @@
 package com.mediasmiths.foxtel.mayam.adapter.service;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import com.mediasmiths.foxtel.mayam.adapter.model.MaterialTransferForQCRequest;
 
 @Path("/mayam")
 public interface MayamAdapterRestService
@@ -13,4 +16,10 @@ public interface MayamAdapterRestService
 	@Path("/ping")
 	@Produces("text/plain")
 	public String ping();
+	
+	@PUT
+	@Path("/material/transferforqc")
+	@Produces("text/plain")
+	public Boolean transferMaterialForQC(MaterialTransferForQCRequest req);
+	
 }
--- fx-MayamAdapter/src/main/java/com/mediasmiths/foxtel/mayam/adapter/service/MayamAdapterRestServiceImpl.java
+++ fx-MayamAdapter/src/main/java/com/mediasmiths/foxtel/mayam/adapter/service/MayamAdapterRestServiceImpl.java
@@ -1,8 +1,14 @@
 package com.mediasmiths.foxtel.mayam.adapter.service;
 
+import java.net.URI;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+
+import com.mediasmiths.foxtel.mayam.adapter.model.MaterialTransferForQCRequest;
 
 public class MayamAdapterRestServiceImpl implements MayamAdapterRestService
 {
@@ -13,6 +19,17 @@ public class MayamAdapterRestServiceImpl implements MayamAdapterRestService
 	public String ping()
 	{
 		return "ping";
+	}
+
+	@PUT
+	@Path("/material/transferforqc")
+	@Produces("text/plain")
+	public Boolean transferMaterialForQC(MaterialTransferForQCRequest req)
+	{
+		final String materialID = req.getMaterialID();
+		final URI destinaion = req.getDestination();
+		
+		return Boolean.TRUE;
 	}
 
 }
