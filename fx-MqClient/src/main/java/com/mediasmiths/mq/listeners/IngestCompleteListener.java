322745494efa5abf1a2469642d39460bfbb59fd7
David Gault <david.gault@mediasmiths.com>
2012-11-05 11:55:55 +0000

    Updating Asset Creation and Ingest Complete listeners

--- fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/IngestCompleteListener.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/IngestCompleteListener.java
@@ -6,7 +6,6 @@ import com.mayam.wf.attributes.shared.type.TaskState;
 import com.mayam.wf.mq.MqMessage;
 import com.mayam.wf.mq.Mq.Listener;
 import com.mayam.wf.mq.common.ContentTypes;
-import com.mayam.wf.attributes.shared.type.AssetType;
 import com.mediasmiths.mayam.MayamAssetType;
 import com.mediasmiths.mayam.MayamTaskListType;
 import com.mediasmiths.mayam.controllers.MayamTaskController;
@@ -31,41 +30,15 @@ public class IngestCompleteListener {
 						{
 							messageAttributes.setAttribute(Attribute.TASK_STATE, TaskState.REMOVED);
 							taskController.saveTask(messageAttributes);
-								
+							
 							String assetID = messageAttributes.getAttribute(Attribute.ASSET_ID);
 							String assetType = messageAttributes.getAttribute(Attribute.ASSET_TYPE);
-							String parentID = "";
-							//TODO: Parent ID not yet implemented
-							//parentID = messageAttributes.getAttribute(Attribute.ASSET_PARENT_ID);
 							
-							boolean qcRequired = false;
-							
-							if (qcRequired){
-								//TODO: Implement QC condition
-								long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.QC_VIEW);
-								AttributeMap newTask = taskController.getTask(taskID);
-								newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
-								taskController.saveTask(newTask);
-							}
-							else if (assetType.equals(AssetType.ITEM) && (parentID != null || parentID.equals(""))) 
-							{
-								//Skip QC and move straight to compliance	
-								//Compile flag is not stored in Mayam, but Parent Id is taken from compiled object
-								//As such we are using the presence of Parent Id on a Material asset to determined if compile was set
-								long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.COMPLIANCE_LOGGING);
-								AttributeMap newTask = taskController.getTask(taskID);
-								newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
-								taskController.saveTask(newTask);
-							}
-							else {
-								//Skip both compliance and QC flows and go straight to segmentation
-								long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.SEGMENTATION);
-								AttributeMap newTask = taskController.getTask(taskID);
-								newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
-								taskController.saveTask(newTask);
-							}
-							
-						}	
+							long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.QC_VIEW);
+							AttributeMap newTask = taskController.getTask(taskID);
+							newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
+							taskController.saveTask(newTask);
+						}
 					}
 				}
 			}
--- fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/ItemCreationListener.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/listeners/ItemCreationListener.java
@@ -2,6 +2,7 @@ package com.mediasmiths.mq.listeners;
 
 import com.mayam.wf.attributes.shared.Attribute;
 import com.mayam.wf.attributes.shared.AttributeMap;
+import com.mayam.wf.attributes.shared.type.AssetType;
 import com.mayam.wf.attributes.shared.type.TaskState;
 import com.mayam.wf.mq.MqMessage;
 import com.mayam.wf.mq.Mq.Listener;
@@ -21,22 +22,31 @@ public class ItemCreationListener
 			{
 				if (msg.getType().equals(ContentTypes.ATTRIBUTES)) 
 				{
-				    //If the Item was created and the Master message had the Compile flag set, 
-					//the WFE will add the Item to the Compliance Logging Worklist
 					AttributeMap messageAttributes = msg.getSubject();
 					String assetID = messageAttributes.getAttribute(Attribute.ASSET_ID);
 						
 					//TODO: Need to confirm check for already existing asset
 					if (assetID == null || assetID.equals(""))
 					{			
-						client.createAsset(messageAttributes);
 						String assetType = messageAttributes.getAttribute(Attribute.ASSET_TYPE);
-
-						long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.INGEST);
-						AttributeMap newTask = taskController.getTask(taskID);
-						newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
-						taskController.saveTask(newTask);
-
+						String parentID = messageAttributes.getAttribute(Attribute.ASSET_PARENT_ID);
+						
+						if (assetType.equals(AssetType.ITEM) && (parentID != null)) 
+						{
+							//Skip QC and move straight to compliance	
+							//Compile flag is not stored in Mayam, but Parent Id is taken from compiled object
+							//As such we are using the presence of Parent Id on a Material asset to determined if compile was set
+							long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.COMPLIANCE_LOGGING);
+							AttributeMap newTask = taskController.getTask(taskID);
+							newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
+							taskController.saveTask(newTask);
+						}
+						else {
+							long taskID = taskController.createTask(assetID, MayamAssetType.fromString(assetType), MayamTaskListType.INGEST);
+							AttributeMap newTask = taskController.getTask(taskID);
+							newTask.setAttribute(Attribute.TASK_STATE, TaskState.OPEN);
+							taskController.saveTask(newTask);
+						}
 					}
 				}
 			}
