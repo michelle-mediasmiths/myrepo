916df077ccbcbd1b7bdf2d9c8277abd020525496
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-11-07 12:43:20 +0000

    new types in wfadapter for tx delivery flow

---
+++ fx-WFAdapter/src/main/java/com/mediasmiths/foxtel/wf/adapter/model/InvokeIntalioTXFlow.java
@@ -0,0 +1,21 @@
+package com.mediasmiths.foxtel.wf.adapter.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class InvokeIntalioTXFlow
+{
+	private String packageID;
+
+	@XmlElement(required = true)
+	public String getPackageID()
+	{
+		return packageID;
+	}
+
+	public void setPackageID(String packageID)
+	{
+		this.packageID = packageID;
+	}
+}
