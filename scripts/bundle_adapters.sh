26fa1a75956dc0d283f44d65265763fc758babb9
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-12-12 10:20:38 +1100

    add bunble adapters script

---
+++ scripts/bundle_adapters.sh
@@ -0,0 +1,19 @@
+#!/bin/bash --
+
+mkdir /tmp/bundle
+rm /tmp/bundle/*
+
+cp ../fx-PlaceholderAgent/target/fx-PlaceholderAgent.war /tmp/bundle
+cp ../fx-MediaPickupAgent/target/fx-MediaPickupAgent.war /tmp/bundle
+cp ../fx-TcAdapter/target/fx-TcAdapter.war /tmp/bundle
+cp ../fx-qcAdapter/target/fx-qcAdapter.war /tmp/bundle
+cp ../fx-WFAdapter/target/fx-WFAdapter.war /tmp/bundle
+cp ../fx-FSAdapter/target/fx-FSAdapter.war /tmp/bundle
+cp ../fx-MqClient/target/fx-MqClient-0.0.2-SNAPSHOT.war /tmp/bundle/fx-MqClient.war
+
+
+cd /tmp/bundle
+tar -cvzf mediasmiths-wfe-bundle.tar.gz *
+
+
+
