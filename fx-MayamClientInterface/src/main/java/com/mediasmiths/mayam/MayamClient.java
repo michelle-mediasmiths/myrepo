777396619978cb24bca7c44f39f3db413b014c17
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-12-12 11:18:08 +1100

    add method comments

--- fx-MayamClientInterface/src/main/java/com/mediasmiths/mayam/MayamClient.java
+++ fx-MayamClientInterface/src/main/java/com/mediasmiths/mayam/MayamClient.java
@@ -24,50 +24,161 @@ public interface MayamClient
 
 	public void shutdown();
 
-	/* titles */
+	/**
+	 * Creates a title, called by placeholder management agent
+	 * @param title
+	 * @return
+	 */
 	public MayamClientErrorCode createTitle(CreateOrUpdateTitle title);
 
+	/**
+	 * Creates a title using the information in a marketing material message
+	 * 
+	 * called by media pickup agent
+	 * 
+	 * @param title
+	 * @return
+	 */
 	public MayamClientErrorCode createTitle(Title title); // may be called when marketing material arrives with no placeholder
 
+	/**
+	 * updates title information from a material exchange message
+	 * @param title
+	 * @return
+	 */
 	public MayamClientErrorCode updateTitle(Material.Title title);
 
+	/**
+	 * updates a title from a placeholder management message
+	 * @param title
+	 * @return
+	 */
 	public MayamClientErrorCode updateTitle(CreateOrUpdateTitle title);
 
+	/**
+	 * actions a purge title request (called by placeholder management agent)
+	 * @param title
+	 * @return
+	 */
 	public MayamClientErrorCode purgeTitle(PurgeTitle title);
 
-	public Title getTitle(String titleID, boolean includeVersionInfo) throws MayamClientException; // returns representation of title used in mediaexchange schema, includeVersionInfo is false when called as part of tx delivery as only information for the package being delivered is required
+	/**
+	 * 
+	 * returns representation of title used in mediaexchange schema, includeVersionInfo is false when called as part of tx delivery as only information for the package being delivered is required
+	 * 
+	 * @param titleID
+	 * @param includeVersionInfo
+	 * @return
+	 * @throws MayamClientException
+	 */
+	public Title getTitle(String titleID, boolean includeVersionInfo) throws MayamClientException;
 
-	public String getTitleOfPackage(String packageID) throws MayamClientException; // returns the title id a given package is associated with
+	/**
+	 * returns the title id a given package is associated with
+	 * 
+	 * @param packageID
+	 * @return
+	 * @throws MayamClientException
+	 */
+	public String getTitleOfPackage(String packageID) throws MayamClientException;
 
+	/**
+	 * returns true if a title with the given id exists
+	 * @param titleID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public boolean titleExists(String titleID) throws MayamClientException;
 
-	/* material */
+	/**
+	 * Create a material using the placeholder management MaterialType
+	 * @param material
+	 * @param titleID
+	 * @return
+	 */
 	public MayamClientErrorCode createMaterial(MaterialType material, String titleID);
-
+	
+	/**
+	 * Create a material using the material exchange MarketingMaterialType
+	 * @param titleID
+	 * @param material
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public String createMaterial(String titleID, MarketingMaterialType material) throws MayamClientException;
 
+	/**
+	 * update a material using the material exchange ProgrammeMaterialType
+	 * @param material
+	 * @return
+	 */
 	public MayamClientErrorCode updateMaterial(ProgrammeMaterialType material);
-
+	/**
+	 * update a material using the placeholder management MaterialType
+	 * @param material
+	 * @return
+	 */
 	public MayamClientErrorCode updateMaterial(MaterialType material);
 
+	/**
+	 * actions a placeholder management delete material message
+	 * @param deleteMaterial
+	 * @return
+	 */
 	public MayamClientErrorCode deleteMaterial(DeleteMaterial deleteMaterial);
 
+	/**
+	 * returns true if a material with the given id exists
+	 * @param materialID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public boolean materialExists(String materialID) throws MayamClientException;
 	
 	public Details getSupplierDetails(String materialID) throws MayamClientException; //called when producing companion xml for tx delivery
 
-	// returns true if the specified material has not had media\essence ingested
+	/**
+	 * returns true if the specified material has not had media\essence ingested
+	 * @param materialID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public boolean isMaterialPlaceholder(String materialID) throws MayamClientException;
 
-	/* packages */
+	/**
+	 * Creates a tx package from the placeholder management PackageType	
+	 * @param txPackage
+	 * @return
+	 */
 	public MayamClientErrorCode createPackage(PackageType txPackage);
 
+	/**
+	 * updates a tx package using the placeholder management PackageType
+	 * @param txPackage
+	 * @return
+	 */
 	public MayamClientErrorCode updatePackage(PackageType txPackage);
-
+	
+	/**
+	 * updates a tx package using the material exchange ProgrammeMaterialType.Presentation.Package type
+	 * @param txPackage
+	 * @return
+	 */
 	public MayamClientErrorCode updatePackage(ProgrammeMaterialType.Presentation.Package txPackage);
 
+	/**
+	 * actions a placeholder management delete package message
+	 * @param deletePackage
+	 * @return
+	 */
 	public MayamClientErrorCode deletePackage(DeletePackage deletePackage);
 
+	/**
+	 * returns true if a package with the given presenetationID exists
+	 * @param presentationID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public boolean packageExists(String presentationID) throws MayamClientException;
 
 	public boolean isMaterialForPackageProtected(String packageID) throws MayamClientException;
@@ -76,33 +187,103 @@ public interface MayamClient
 
 	public MayamValidator getValidator();
 
+	/**
+	 * Returns a list of channels that have licences for this material
+	 * @param materialID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	public ArrayList<String> getChannelLicenseTagsForMaterial(String materialID) throws MayamClientException;
 
 	/**
+	 * 
+	 * Returns the file path to the high resolution media attatched to a material 
+	 * 
 	 * @param materialID
 	 * @param location
 	 * @return 
 	 */
 	public String pathToMaterial(String materialID) throws MayamClientException;
 
+	/**
+	 * Return the placeholder management system representation of a package
+	 * @param packageID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	PackageType getPackage(String packageID) throws MayamClientException;
 
+	/**
+	 * Return the material exchange representation of a package
+	 * @param packageID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	ProgrammeMaterialType.Presentation.Package getPresentationPackage(String packageID) throws MayamClientException;
 
+	/**
+	 * Returns the MaterialType representation of a given material
+	 * @param materialID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	MaterialType getMaterial(String materialID) throws MayamClientException;
 
-	public AttributeMap getTaskForAsset(MayamTaskListType type, String id) throws MayamClientException;
+	/**
+	 * Return a single task of a given type for the specified asset
+	 * @param type
+	 * @param id
+	 * @return
+	 * @throws MayamClientException
+	 */
+	public AttributeMap getTaskForAsset(MayamTaskListType type, String assetid) throws MayamClientException;
 
+	/**
+	 * Save the given task
+	 * @param task
+	 * @throws MayamClientException
+	 */
 	public void saveTask(AttributeMap task) throws MayamClientException;
 
-	public void failTaskForAsset(MayamTaskListType txDelivery, String id) throws MayamClientException;
+	/**
+	 * Called to indicate that a given task for an asset should be considered failed or erronous
+	 * @param txDelivery
+	 * @param id
+	 * @throws MayamClientException
+	 */
+	public void failTaskForAsset(MayamTaskListType taskType, String id) throws MayamClientException;
 
+	/**
+	 * returns the ProgrammeMaterial representation of a given material
+	 * @param materialID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	ProgrammeMaterialType getProgrammeMaterialType(String materialID) throws MayamClientException;
 
+	/**
+	 * Returns the material id that a given package is associated with
+	 * @param packageID
+	 * @return
+	 * @throws MayamClientException
+	 */
 	String getMaterialIDofPackageID(String packageID) throws MayamClientException;
 
+	/**
+	 * called by wfapapter when it learns of a tx delivery failure
+	 * @param packageID
+	 * @param failureReason
+	 * @throws MayamClientException
+	 */
+	
 	public void createTxDeliveryFailureTask(String packageID, String failureReason) throws MayamClientException;
 
-	public void updateMaterial(DetailType details, String materialID);
+	/**
+	 * updates a material based on the ruzz ingest DetailsType
+	 * @param details
+	 * @param materialID
+	 * @throws MayamClientException
+	 */
+	public void updateMaterial(DetailType details, String materialID) throws MayamClientException;
 
 }
