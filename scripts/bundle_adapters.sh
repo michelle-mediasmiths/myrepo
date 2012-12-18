c0d1469ee7fbe08976a2d5afbb261daff4ea7223
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2012-12-18 11:00:56 +0000

    update version number in script

--- scripts/bundle_adapters.sh
+++ scripts/bundle_adapters.sh
@@ -9,7 +9,7 @@ cp ../fx-TcAdapter/target/fx-TcAdapter.war /tmp/bundle
 cp ../fx-qcAdapter/target/fx-qcAdapter.war /tmp/bundle
 cp ../fx-WFAdapter/target/fx-WFAdapter.war /tmp/bundle
 cp ../fx-FSAdapter/target/fx-FSAdapter.war /tmp/bundle
-cp ../fx-MqClient/target/fx-MqClient-0.0.2-SNAPSHOT.war /tmp/bundle/fx-MqClient.war
+cp ../fx-MqClient/target/fx-MqClient-3.0-SNAPSHOT.war /tmp/bundle/fx-MqClient.war
 
 
 cd /tmp/bundle
