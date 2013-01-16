17849d773343159b8ec8857818887185741577d2
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-01-16 09:24:01 +0000

    remove old carbon coder client

--- fx-CarbonCoderClient/src/main/java/com/mediasmiths/foxtel/carbon/jaxb/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlAccessorType(XmlAccessType.NONE)
-package com.mediasmiths.foxtel.carbon.jaxb;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
--- fx-CarbonCoderClient/src/main/java/com/mediasmiths/foxtel/carbon/profile/ProfileType.java
+++
@@ -1,6 +0,0 @@
-package com.mediasmiths.foxtel.carbon.profile;
-
-public enum ProfileType
-{
-	Destination,Filter_Audio,Filter_Video,Setting,Connection,System,Seachange;
-}
