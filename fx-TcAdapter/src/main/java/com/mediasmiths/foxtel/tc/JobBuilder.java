bbdb84e09d580adda898a1a65f5bab6d7e6f67b0
Peter Wright <peter.wright@mediasmiths.com>
2013-01-18 14:52:16 +0000

    Autoformat, also documenting the TxProfiles enums clearly

--- fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/JobBuilder.java
+++ fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/JobBuilder.java
@@ -1,13 +1,8 @@
 package com.mediasmiths.foxtel.tc;
 
-import java.io.IOException;
-import org.apache.cxf.helpers.IOUtils;
-import org.apache.log4j.Logger;
-
 import au.com.foxtel.cf.mam.pms.MaterialType;
 import au.com.foxtel.cf.mam.pms.PackageType;
 import au.com.foxtel.cf.mam.pms.PresentationFormatType;
-
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.mediasmiths.foxtel.pathresolver.PathResolver;
@@ -16,18 +11,15 @@ import com.mediasmiths.foxtel.pathresolver.UnknownPathException;
 import com.mediasmiths.foxtel.tc.model.TCBuildJobXMLRequest;
 import com.mediasmiths.mayam.MayamClient;
 import com.mediasmiths.mayam.MayamClientException;
+import org.apache.cxf.helpers.IOUtils;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 public class JobBuilder
 {
 
 	private final static Logger log = Logger.getLogger(JobBuilder.class);
-
-	/**
-	 * 
-	 * video audio dolbyEe downres profile SD Stereo No n/a MAM-SD-12ST_GXF-SD-12ST SD Stereo + Descrete Surround Yes n/a MAM-SD-12ST_38SUR_GXF-SD-12ST-34DBE HD Stereo No No MAM-HD-12ST_GXF-HD-12ST HD
-	 * Stereo + Descrete Surround Yes No MAM-HD-12ST_38SUR_GXF-HD-12ST-34DBE HD Stereo No Yes MAM-HD-12ST_GXF-SD-12ST HD Stereo + Descrete Surround Yes Yes MAM-HD-12ST_38SUR_GXF-SD-12ST-34DBE
-	 * 
-	 */
 
 	@Inject
 	private MayamClient mayamClient;
@@ -38,16 +30,73 @@ public class JobBuilder
 	@Inject
 	@Named(Config.BUG_FOLDER)
 	private String buglocation;
-	
+
+
+	/**
+	 * Describes the base profiles
+	 */
 	public enum TxProfile
 	{
+		/**
+		 * HD GXF with Stereo audio
+		 * <ul>
+		 * <li>GXF</li>
+		 * <li>HD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * </ul>
+		 */
 		MAM_HD_12ST_GXF_HD_12ST("pcp/quicktime.xml"),
+		/**
+		 * SD GXF (from HD source) with Stereo audio
+		 * <ul>
+		 * <li>GXF</li>
+		 * <li>Downres HD to SD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * </ul>
+		 */
 		MAM_HD_12ST_GXF_SD_12ST("pcp/quicktime.xml"),
+		/**
+		 * SD GXF with Stereo audio
+		 * <ul>
+		 * <li>GXF</li>
+		 * <li>SD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * </ul>
+		 */
+		MAM_SD_12ST_GXF_SD_12ST("pcp/quicktime.xml"),
+
+		/**
+		 * HD GXF with Stereo and DolbyE
+		 * <ul>
+		 * <li>GXF</li>
+		 * <li>HD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * <li>A3 and A4 are DolbyE</li>
+		 * </ul>
+		 */
 		MAM_HD_12ST_38SUR_GXF_HD_12ST_34DBE("pcp/quicktime.xml"),
+
+		/**
+		 * SD GXF (from HD source) with Stereo and DolbyE
+		 * <ul>
+		 * <li>GXF</li>
+		 * <li>Downres HD to SD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * <li>A3 and A4 are DolbyE</li>
+		 * </ul>
+		 */
 		MAM_HD_12ST_38SUR_GXF_SD_12ST_34DBE("pcp/quicktime.xml"),
-		MAM_SD_12ST_38SUR_GXF_SD_12ST_34DBE("pcp/quicktime.xml"),
-		MAM_SD_12ST_GXF_SD_12ST("pcp/quicktime.xml");	
-		
+
+		/**
+		 * SD GXF with Stereo and DolbyE
+		 * <ul>
+		 * <li>SD</li>
+		 * <li>A1 and A2 are Stereo tracks</li>
+		 * <li>A3 and A4 are DolbyE</li>
+		 * </ul>
+		 */
+		MAM_SD_12ST_38SUR_GXF_SD_12ST_34DBE("pcp/quicktime.xml"),;
+
 		private String filename;
 
 		TxProfile(String fname)
@@ -67,16 +116,15 @@ public class JobBuilder
 	private final static String FULL_UNC_OUTPUT_PATH_PH = "FULL_UNC_OUTPUT_PATH";
 	private final static String OUTPUT_BASENAME_PH = "OUTPUT_BASENAME";
 
-	public String buildJobForTxPackageTranscode(String packageID, String inputfile, String outputFolder)
-			throws MayamClientException,
-			JobBuilderException, UnknownPathException
+	public String buildJobForTxPackageTranscode(String packageID,
+	                                            String inputfile,
+	                                            String outputFolder) throws MayamClientException, JobBuilderException, UnknownPathException
 	{
 
-		log.info(String.format(
-				"Building job for tx package transcode packageId %s inputfile %s outputfolder %s",
-				packageID,
-				inputfile,
-				outputFolder));
+		log.info(String.format("Building job for tx package transcode packageId %s inputfile %s outputfolder %s",
+		                       packageID,
+		                       inputfile,
+		                       outputFolder));
 		// fetch package and material information to determine profile
 		PackageType pack = mayamClient.getPackage(packageID);
 		MaterialType material = mayamClient.getMaterial(pack.getMaterialID());
@@ -98,23 +146,25 @@ public class JobBuilder
 		String pcp = loadProfileForPackage(packageID, profile);
 		pcp = setInputAndOutputPaths(packageID, inputfile, outputFolder, pcp);
 
-		
-		
+
 		return pcp;
 	}
 
-	private String setInputAndOutputPaths(String packageID, String inputfile, String outputFolder, String pcp) throws UnknownPathException
+	private String setInputAndOutputPaths(String packageID,
+	                                      String inputfile,
+	                                      String outputFolder,
+	                                      String pcp) throws UnknownPathException
 	{
 		// assuming for now that the path is a linux style path	
 		String uncInputPath = pathResolver.uncPath(PathType.NIX, inputfile);
 		String uncOutputPath = pathResolver.uncPath(PathType.NIX, outputFolder);
-		
+
 		pcp = pcp.replace(INPUT_FILE_PATH_PH, uncInputPath);
 		pcp = pcp.replace(OUTPUT_FOLDER_PATH_PH, uncOutputPath);
-		
+
 		pcp = pcp.replace(FULL_UNC_INPUT_PATH_PH, uncInputPath);
 		pcp = pcp.replace(FULL_UNC_OUTPUT_PATH_PH, uncOutputPath);
-		
+
 		pcp = pcp.replace(OUTPUT_BASENAME_PH, packageID);
 		return pcp;
 	}
@@ -201,36 +251,43 @@ public class JobBuilder
 	private final static long ONE_DAY = ONE_HOUR * 24;
 	private final static long THREE_DAYS = ONE_DAY * 3;
 	private final static long EIGHT_DAYS = ONE_DAY * 8;
-	
-	
+
+
 	public Integer getPriorityForTXJob(TCBuildJobXMLRequest buildJobXMLRequest)
 	{
-		log.debug(String.format("determining prority for job for asset %s tx date is %s",buildJobXMLRequest.getPackageID(),buildJobXMLRequest.getTxDate().toString()));
-	
-		int priority=1;
-		
+		log.debug(String.format("determining prority for job for asset %s tx date is %s",
+		                        buildJobXMLRequest.getPackageID(),
+		                        buildJobXMLRequest.getTxDate().toString()));
+
+		int priority = 1;
+
 		long now = System.currentTimeMillis();
 		long txTime = buildJobXMLRequest.getTxDate().getTime();
 		long difference = txTime - now;
-		
-		if(difference < TWELVE_HOURS){
-			priority=8;
+
+		if (difference < TWELVE_HOURS)
+		{
+			priority = 8;
 		}
-		else if(difference < ONE_DAY){
-			priority=7;
+		else if (difference < ONE_DAY)
+		{
+			priority = 7;
 		}
-		else if(difference < THREE_DAYS){
-			priority=6;
-		}		
-		else if(difference < EIGHT_DAYS){
-			priority=4;
-		}	
-		else{
-			priority=2;
+		else if (difference < THREE_DAYS)
+		{
+			priority = 6;
 		}
-		
-		log.debug("returning prority "+priority);
-		return Integer.valueOf(priority);
+		else if (difference < EIGHT_DAYS)
+		{
+			priority = 4;
+		}
+		else
+		{
+			priority = 2;
+		}
+
+		log.debug("returning prority " + priority);
+		return priority;
 	}
 
 }
