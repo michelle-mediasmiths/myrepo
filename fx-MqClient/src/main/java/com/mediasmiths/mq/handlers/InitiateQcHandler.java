da915294c818dff175a7fb9831bee1f1db25d105
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-01-22 17:52:13 +0000

    FX-640 cancel qc tasks created for items that have passed preview

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/InitiateQcHandler.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/InitiateQcHandler.java
@@ -7,7 +7,9 @@ import com.mayam.wf.attributes.shared.AttributeMap;
 import com.mayam.wf.attributes.shared.type.MediaStatus;
 import com.mayam.wf.attributes.shared.type.TaskState;
 import com.mediasmiths.mayam.MayamClientException;
+import com.mediasmiths.mayam.MayamPreviewResults;
 import com.mediasmiths.mayam.MayamTaskListType;
+import com.mediasmiths.mayam.util.AssetProperties;
 
 public class InitiateQcHandler extends AttributeHandler
 {
@@ -25,44 +27,66 @@ public class InitiateQcHandler extends AttributeHandler
 
 				if (mediaStatus.equals(MediaStatus.MISSING))
 				{
-					log.info(String.format("A qc task was created for an item %s with no media, cancelling", messageAttributes.getAttributeAsString(Attribute.HOUSE_ID)));
-					//a qc task has been created for an item with no media, cancel the task
-					messageAttributes.setAttribute(Attribute.TASK_STATE, TaskState.REJECTED);
-					try
-					{
-						taskController.saveTask(messageAttributes);
-					}
-					catch (MayamClientException e)
-					{
-						log.error("error cancelling task",e);
-					}
+					log.info(String.format(
+							"A qc task was created for an item %s with no media, cancelling",
+							messageAttributes.getAttributeAsString(Attribute.HOUSE_ID)));
+					cancelTask(messageAttributes);
 				}
 				else
 				{
-
-					// task just created, lets perform file format verification
-					try
+					String previewStatus = messageAttributes.getAttribute(Attribute.QC_PREVIEW_RESULT);
+					if (MayamPreviewResults.isPreviewPass(previewStatus))
 					{
-						materialController.verifyFileMaterialFileFormat(messageAttributes);
+						log.info(String.format(
+								"A qc task was created for an item %s but it has already passed preview, cancelling",
+								messageAttributes.getAttributeAsString(Attribute.HOUSE_ID)));
+						cancelTask(messageAttributes);
 					}
-					catch (MayamClientException e)
+					else
 					{
-						log.error(
-								"MayamClient exception while performing file format verification for asset"
-										+ messageAttributes.getAttributeAsString(Attribute.ASSET_ID),
-								e);
-						messageAttributes.setAttribute(Attribute.TASK_STATE, TaskState.ERROR);
-						try
-						{
-							taskController.saveTask(messageAttributes);
-						}
-						catch (MayamClientException e1)
-						{
-							log.error("error setting task to error state", e1);
-						}
+						fileformatVerification(messageAttributes);
 					}
 				}
 			}
+		}
+	}
+
+	private void fileformatVerification(AttributeMap messageAttributes)
+	{
+		// task just created, lets perform file format verification
+		try
+		{
+			materialController.verifyFileMaterialFileFormat(messageAttributes);
+		}
+		catch (MayamClientException e)
+		{
+			log.error(
+					"MayamClient exception while performing file format verification for asset"
+							+ messageAttributes.getAttributeAsString(Attribute.ASSET_ID),
+					e);
+			messageAttributes.setAttribute(Attribute.TASK_STATE, TaskState.ERROR);
+			try
+			{
+				taskController.saveTask(messageAttributes);
+			}
+			catch (MayamClientException e1)
+			{
+				log.error("error setting task to error state", e1);
+			}
+		}
+	}
+
+	private void cancelTask(AttributeMap messageAttributes)
+	{
+		// a qc task has been created for an item with no media, cancel the task
+		messageAttributes.setAttribute(Attribute.TASK_STATE, TaskState.REJECTED);
+		try
+		{
+			taskController.saveTask(messageAttributes);
+		}
+		catch (MayamClientException e)
+		{
+			log.error("error cancelling task", e);
 		}
 	}
 
