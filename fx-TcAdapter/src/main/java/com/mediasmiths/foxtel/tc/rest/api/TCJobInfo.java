2fc99b47d0e303417b2e1532c3980faab0783f2f
Peter Wright <peter.wright@mediasmiths.com>
2013-01-24 16:54:44 +0000

    Adding missing jaxb annotations

--- fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/rest/api/TCJobInfo.java
+++ fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/rest/api/TCJobInfo.java
@@ -2,7 +2,11 @@ package com.mediasmiths.foxtel.tc.rest.api;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlType
+@XmlRootElement(name = "JobInfo")
 public class TCJobInfo
 {
 	@XmlAttribute
--- fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/rest/api/TCJobResult.java
+++ fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/rest/api/TCJobResult.java
@@ -1,5 +1,8 @@
 package com.mediasmiths.foxtel.tc.rest.api;
 
+import javax.xml.bind.annotation.XmlEnum;
+
+@XmlEnum
 public enum TCJobResult
 {
 	SUCCESS,
