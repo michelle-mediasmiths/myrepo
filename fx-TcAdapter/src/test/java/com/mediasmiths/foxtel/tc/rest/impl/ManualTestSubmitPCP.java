f72c14b017a107274a8b1d13a495bc6f648df50a
Peter Wright <peter.wright@mediasmiths.com>
2013-01-28 10:16:58 +0000

    adding more documentation to test class, renaming to show that it's a manual test

--- fx-TcAdapter/src/test/java/com/mediasmiths/foxtel/tc/rest/impl/ManualTestSubmitPCP.java
+++ fx-TcAdapter/src/test/java/com/mediasmiths/foxtel/tc/rest/impl/ManualTestSubmitPCP.java
@@ -2,6 +2,7 @@ package com.mediasmiths.foxtel.tc.rest.impl;
 
 import com.google.inject.Injector;
 import com.mediasmiths.foxtel.tc.rest.api.TCAudioType;
+import com.mediasmiths.foxtel.tc.rest.api.TCJobInfo;
 import com.mediasmiths.foxtel.tc.rest.api.TCJobParameters;
 import com.mediasmiths.foxtel.tc.rest.api.TCOutputPurpose;
 import com.mediasmiths.foxtel.tc.rest.api.TCRestService;
@@ -10,10 +11,11 @@ import com.mediasmiths.std.guice.apploader.impl.GuiceInjectorBootstrap;
 import com.mediasmiths.std.guice.restclient.JAXRSProxyClientFactory;
 import com.mediasmiths.std.guice.web.rest.CoreRestServicesModule;
 import com.mediasmiths.std.io.PropertyFile;
+import com.mediasmiths.std.util.jaxb.JAXBSerialiser;
 
 import java.net.URI;
 
-public class TestBuildPCP
+public class ManualTestSubmitPCP
 {
 	public static void main(String[] args) throws Exception
 	{
@@ -30,12 +32,17 @@ public class TestBuildPCP
 		params.audioType = TCAudioType.DOLBY_E;
 		params.purpose = TCOutputPurpose.DVD;
 
-		String xml = svc.createPCPXML(params);
 
+		// Display the generated Project
+		String xml = svc.createPCPXML(params);
 		System.out.println("XML generated: " + xml);
 
+		// Submit the job to WFS
 		String id = svc.createJob(params);
-
 		System.out.println("Job created: " + id);
+
+		// Query the job details and then serialise them
+		TCJobInfo job = svc.queryJob(id);
+		System.out.println(JAXBSerialiser.getInstance(TCJobInfo.class).serialise(job));
 	}
 }
