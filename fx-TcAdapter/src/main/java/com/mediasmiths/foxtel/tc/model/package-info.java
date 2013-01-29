a9f18893d427ac2f7437f16ce8eec71b1c351eb9
Peter Wright <peter.wright@mediasmiths.com>
2013-01-29 11:25:58 +0000

    Remove disused old model

--- fx-TcAdapter/src/main/java/com/mediasmiths/foxtel/tc/model/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(namespace = "http://ns.mediasmiths.com/foxtel/tc/adapter", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED) 
-
-package com.mediasmiths.foxtel.tc.model;
-import javax.xml.bind.annotation.XmlSchema;
-
