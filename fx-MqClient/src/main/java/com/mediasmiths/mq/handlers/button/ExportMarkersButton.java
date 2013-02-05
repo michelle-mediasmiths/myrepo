139e1131c5162d209a4e7e6b6846b7b3a229ba4d
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-02-05 13:28:47 +0000

    update tasks client version, add missing logger

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/ExportMarkersButton.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/ExportMarkersButton.java
@@ -1,5 +1,7 @@
 package com.mediasmiths.mq.handlers.button;
 
+import org.apache.log4j.Logger;
+
 import com.mayam.wf.attributes.shared.AttributeMap;
 import com.mediasmiths.mayam.MayamButtonType;
 import com.mediasmiths.mayam.MayamClientException;
@@ -7,6 +9,8 @@ import com.mediasmiths.mayam.MayamClientException;
 public class ExportMarkersButton extends ButtonClickHandler
 {
 
+	private final static Logger log = Logger.getLogger(ExportMarkersButton.class);
+	
 	@Override
 	protected void buttonClicked(AttributeMap messageAttributes)
 	{
