984817b3a303b25f5c94ee2ac8a8e9feda99a1c3
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-02-05 12:38:53 +0000

    exception handler

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/ExportMarkersButton.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/ExportMarkersButton.java
@@ -2,6 +2,7 @@ package com.mediasmiths.mq.handlers.button;
 
 import com.mayam.wf.attributes.shared.AttributeMap;
 import com.mediasmiths.mayam.MayamButtonType;
+import com.mediasmiths.mayam.MayamClientException;
 
 public class ExportMarkersButton extends ButtonClickHandler
 {
@@ -9,7 +10,14 @@ public class ExportMarkersButton extends ButtonClickHandler
 	@Override
 	protected void buttonClicked(AttributeMap messageAttributes)
 	{
-		materialController.exportMarkers(messageAttributes);
+		try
+		{
+			materialController.exportMarkers(messageAttributes);
+		}
+		catch (MayamClientException e)
+		{
+			log.error("error exporting markers",e);
+		}
 	}
 
 	@Override
