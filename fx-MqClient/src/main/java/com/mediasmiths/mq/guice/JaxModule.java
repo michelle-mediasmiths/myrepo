14e09f67c96345690506d8061439c9aecdfbee3d
Peter Wright <peter.wright@mediasmiths.com>
2013-03-14 12:14:15 +0000

    Flag pieces of this module that @Provide a Marshaller as needing to be removed

--- fx-MqClient/src/main/java/com/mediasmiths/mq/guice/JaxModule.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/guice/JaxModule.java
@@ -1,17 +1,16 @@
 package com.mediasmiths.mq.guice;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
-import org.apache.log4j.Logger;
-import org.xml.sax.SAXException;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.mediasmiths.std.util.jaxb.JAXBSerialiser;
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 public class JaxModule extends AbstractModule
 {
@@ -22,26 +21,6 @@ public class JaxModule extends AbstractModule
 	protected void configure()
 	{
 
-	}
-
-	@Provides
-	@Singleton
-	@Named("wfe.marshaller")
-	Marshaller provideWFEMarshaller(@Named("wfe.context") JAXBContext jc) throws JAXBException, SAXException
-	{
-		Marshaller marshaller = null;
-		try
-		{
-			marshaller = jc.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-		}
-		catch (JAXBException e)
-		{
-			logger.fatal("Could not create marshaller", e);
-			throw e;
-		}
-		return marshaller;
 	}
 
 	@Provides
@@ -95,29 +74,7 @@ public class JaxModule extends AbstractModule
 	@Named("fxcommon.serialiser")
 	JAXBSerialiser providefxcommonJAXBSerialiser(@Named("fxcommon.context") JAXBContext context)
 	{
-
 		return JAXBSerialiser.getInstance(context);
-
-	}
-	
-	@Provides
-	@Singleton
-	@Named("fxcommon.marshaller")
-	Marshaller providefxCommonMarshaller(@Named("fxcommon.context") JAXBContext jc) throws JAXBException, SAXException
-	{
-		Marshaller marshaller = null;
-		try
-		{
-			marshaller = jc.createMarshaller();
-			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-		}
-		catch (JAXBException e)
-		{
-			logger.fatal("Could not create marshaller", e);
-			throw e;
-		}
-		return marshaller;
 	}
 
 	@Provides
@@ -147,7 +104,14 @@ public class JaxModule extends AbstractModule
 		return JAXBSerialiser.getInstance(context);
 
 	}
-	
+
+	/**
+	 * TODO remove this - it WILL fail badly. replace with JAXBSerialiser
+	 * @param jc
+	 * @return
+	 * @throws JAXBException
+	 * @throws SAXException
+	 */
 	@Provides
 	@Singleton
 	@Named("placeholderManagement.marshaller")
@@ -167,5 +131,59 @@ public class JaxModule extends AbstractModule
 		}
 		return marshaller;
 	}
-	
+
+
+	/**
+	 * TODO remove this - it WILL fail badly. replace with JAXBSerialiser
+	 * @param jc
+	 * @return
+	 * @throws JAXBException
+	 * @throws SAXException
+	 */
+	@Provides
+	@Singleton
+	@Named("wfe.marshaller")
+	Marshaller provideWFEMarshaller(@Named("wfe.context") JAXBContext jc) throws JAXBException, SAXException
+	{
+		Marshaller marshaller = null;
+		try
+		{
+			marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		}
+		catch (JAXBException e)
+		{
+			logger.fatal("Could not create marshaller", e);
+			throw e;
+		}
+		return marshaller;
+	}
+
+	/**
+	 * TODO remove this - it WILL fail badly. replace with JAXBSerialiser
+	 * @param jc
+	 * @return
+	 * @throws JAXBException
+	 * @throws SAXException
+	 */
+	@Provides
+	@Singleton
+	@Named("fxcommon.marshaller")
+	Marshaller providefxCommonMarshaller(@Named("fxcommon.context") JAXBContext jc) throws JAXBException, SAXException
+	{
+		Marshaller marshaller = null;
+		try
+		{
+			marshaller = jc.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+		}
+		catch (JAXBException e)
+		{
+			logger.fatal("Could not create marshaller", e);
+			throw e;
+		}
+		return marshaller;
+	}
 }
