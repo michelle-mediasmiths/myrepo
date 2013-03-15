6b7b41d1b562d0e5cf7ac0ca2f3174e330fc3b2d
Terry Harmer <terry.harmer@mediasmiths.com>
2013-03-15 15:37:20 +0000

    Additions to configuration.  Tomcat start up script for clear processing directories. Clearer messages from MayamPDPImpl.

---
+++ configuration/tomStartStop.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+
+pgt=/storage/corp/placeholders/Progratron
+
+
+if [ -d "$pgt" ] ; then
+
+    echo "Remove the PrograTron Processing Items in " $pgt
+
+    for f in $pgt/processing/*.xml ; do
+
+         echo "Moving $f to $pgt";
+
+         mv  $f $pgt
+
+    done
+else
+    echo "PrograTron Folder  " $pgt " does not exist "
+fi
+
+for p in /storage/mam/hires01/drop1/technicolor /storage/mam/hires03/drop3/redbee /storage/mam/hires02/drop2/adults /storage/mam/hires04/drop4/batch/hd /storage/mam/hires04/drop4/batch/sd
+
+	if [ -d "$p" ] ; then
+
+        echo "Remove the $p Processing Items"
+
+         for f in $p/processing/* ; do
+
+             if [ ! -d $f ] ; then
+
+                 echo "Moving $f to $p";
+
+                mv  $f $p
+             fi
+
+         done
+else
+    echo "Folder  " $p " does not exist "
+fi
+
+done
