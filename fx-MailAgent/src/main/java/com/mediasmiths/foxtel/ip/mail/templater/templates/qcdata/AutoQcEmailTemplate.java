22f82782b5d1acdf9758bb7e4f903bba441e4ea0
Minh Pham <minhpham@mediasmithss-iMac.local>
2013-03-20 15:26:07 +1100

    MAM-122 update templates and envents

--- fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/qcdata/AutoQcEmailTemplate.java
+++ fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/qcdata/AutoQcEmailTemplate.java
@@ -1,7 +1,7 @@
 package com.mediasmiths.foxtel.ip.mail.templater.templates.qcdata;
 
 import com.mediasmiths.foxtel.ip.common.email.MailTemplate;
-import com.mediasmiths.foxtel.ip.common.events.AutoQCResultNotification;
+import com.mediasmiths.foxtel.ip.common.events.AutoQCFailureNotification;
 import com.mediasmiths.foxtel.ip.mail.templater.EmailTemplateGenerator;
 
 public class AutoQcEmailTemplate extends MailTemplate implements EmailTemplateGenerator
@@ -10,7 +10,7 @@ public class AutoQcEmailTemplate extends MailTemplate implements EmailTemplateGe
 	@Override
 	public boolean handles(Object obj)
 	{
-		return obj instanceof AutoQCResultNotification;
+		return obj instanceof AutoQCFailureNotification;
 	}
 
 	@Override
@@ -19,11 +19,12 @@ public class AutoQcEmailTemplate extends MailTemplate implements EmailTemplateGe
 
 		MailTemplate t = new MailTemplate();
 
-		AutoQCResultNotification aqce = (AutoQCResultNotification) obj;
+		AutoQCFailureNotification aqce = (AutoQCFailureNotification) obj;
 
 		t.setEmailaddresses(getEmailaddresses());
 		t.setSubject(String.format(getSubject(), aqce.getJobId(), aqce.getAssetId(), aqce.getTitle()));
 		t.setBody(String.format(getBody(),  aqce.getAssetId()));
+
 		return t;
 
 	}
