c5cd2425edd1fa62d2d9861d534b7d739a9db72a
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-03-27 11:36:33 +0000

    add parenthises to condition

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/export/ExportProxyButton.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/button/export/ExportProxyButton.java
@@ -100,7 +100,7 @@ public abstract class ExportProxyButton extends ButtonClickHandler
 
 			// If 'No Bug' has been selected then ignore any text in the bug location field
 			Boolean noBug = requestAttributes.getAttribute(Attribute.VISUAL_BUG_FLAG);
-			if (noBug != null && noBug.booleanValue()==true); 
+			if (noBug != null && (noBug.booleanValue()==true)); 
 			{
 				log.debug("'no bug' option was selected");
 				buglocation = null;
