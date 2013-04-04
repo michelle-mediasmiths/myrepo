b2fee8e3b5ed01968ff0f2e0304c1bfe709ac979
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-04-04 15:47:14 +0100

    MAM-319 filter purge emails by channel groups if present

--- fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/content/PurgeEmailTemplate.java
+++ fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/content/PurgeEmailTemplate.java
@@ -1,8 +1,12 @@
 package com.mediasmiths.foxtel.ip.mail.templater.templates.content;
 
 
+import java.util.List;
+
+import com.mediasmiths.foxtel.ip.common.email.Emailaddresses;
 import com.mediasmiths.foxtel.ip.common.email.MailTemplate;
 import com.mediasmiths.foxtel.ip.common.events.PurgeNotification;
+import com.mediasmiths.foxtel.ip.mail.templater.EmailListGroupFilter;
 import com.mediasmiths.foxtel.ip.mail.templater.EmailTemplateGenerator;
 
 public class PurgeEmailTemplate extends MailTemplate implements EmailTemplateGenerator
@@ -20,6 +24,11 @@ public class PurgeEmailTemplate extends MailTemplate implements EmailTemplateGen
 		PurgeNotification ajf = (PurgeNotification) obj;
 		MailTemplate t = new MailTemplate();
 
+
+		Emailaddresses emailaddresses = getEmailaddresses();
+		List<String> channelGroups = ajf.getChannelGroup();
+		
+		t.setEmailaddresses(EmailListGroupFilter.filterByGroups(channelGroups, emailaddresses));
 		t.setSubject(String.format(getSubject(), ajf.getAssetType(), ajf.getHouseId()));
 		t.setBody(getBody() + ", at " + ajf.getTime());
 		t.setEmailaddresses(getEmailaddresses());
