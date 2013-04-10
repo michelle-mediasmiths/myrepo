17bfbdc6b96094bbdf542ccd2892d6bb7038d0ec
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-04-10 12:35:52 +0100

    multipickup : unmatched

---
+++ fx-MediaPickupAgent/src/main/java/com/mediasmiths/foxtel/mpa/queue/FilesPendingUnmatchImport.java
@@ -0,0 +1,11 @@
+package com.mediasmiths.foxtel.mpa.queue;
+
+import java.io.File;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class FilesPendingUnmatchImport extends LinkedBlockingQueue<File>
+{
+
+	private static final long serialVersionUID = 1L;
+
+}
