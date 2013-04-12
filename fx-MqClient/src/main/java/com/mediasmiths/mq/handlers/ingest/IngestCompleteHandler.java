30e192556cf594158dde5e0746a0851dc9a37205
Minh Pham <minhpham@mediasmithss-iMac.local>
2013-04-12 16:54:56 +1000

    MAM-79 use values of medst_hr and medst_lr

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/ingest/IngestCompleteHandler.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/ingest/IngestCompleteHandler.java
@@ -9,70 +9,89 @@ import com.mayam.wf.attributes.shared.type.MediaStatus;
 import com.mediasmiths.mayam.MayamAssetType;
 import com.mediasmiths.mayam.MayamClientErrorCode;
 import com.mediasmiths.mayam.MayamClientException;
-import com.mediasmiths.mayam.util.AssetProperties;
-import com.mediasmiths.mq.handlers.UpdateAttributeHandler;
+import com.mayam.wf.attributes.shared.type.TaskState;
+import com.mayam.wf.attributes.shared.type.QcStatus;
+import com.mediasmiths.mayam.MayamTaskListType;
+import com.mediasmiths.mq.handlers.TaskUpdateHandler;
 
-//NOTE: it appears that the MEDST_LR and MEDST_HR attributes required for this logic are not included in asset update notifications
-//The required attributes are on task update messages so this listener needs changed to act as a guard on qc task processing, but not creation.
 
-/**
- * 
- * Monitors for the conditions required to consider in ingest complete. One would think it would be an 'ingest' job finishing for a given item but its not
- * 
- * According to viz we cant start qc until lowres on central is closed + hires on central is closed
- * 
- * For most ingests these things happen before and ingest job finished message is sent from ardome, but not always
- * 
- * MAM-79
- * 
- * QC not to be done until:
- * 
- * MEDST_LR = Ready MEDST_HR = Ready AND there is a storage url in the hires storage
- * 
- */
-public class IngestCompleteHandler extends UpdateAttributeHandler
+
+public class IngestCompleteHandler extends TaskUpdateHandler
 {
 
 	private final static Logger log = Logger.getLogger(IngestCompleteHandler.class);
+	
+	@Override
+	public String getName()
+	{
+		return "IngestTaskUpdate";
+	}
+
 
 	@Override
-	public void process(AttributeMap currentAttributes, AttributeMap before, AttributeMap after)
+	public void onTaskUpdate(AttributeMap currentAttributes, AttributeMap before, AttributeMap after)
 	{
 		AssetType assetType = currentAttributes.getAttribute(Attribute.ASSET_TYPE);
 
 		if (!assetType.equals(MayamAssetType.MATERIAL.getAssetType()))
 		{
-			return; // only interested in materials\items
+			return; 
 		}
 
 		String assetID = currentAttributes.getAttribute(Attribute.ASSET_ID);
 		String houseID = currentAttributes.getAttribute(Attribute.HOUSE_ID);
+		
+		QcStatus qcStatus = currentAttributes.getAttribute(Attribute.QC_STATUS);
 
-		if (AssetProperties.hasQCStatus(currentAttributes))
+		if ((null != qcStatus) && !qcStatus.equals(QcStatus.TBD))
 		{
 			log.debug("asset already has a non TBD qc status");
-			return; // asset has had qc performed on it already, it cant have just been ingested
+			return; 
 		}
 
 		boolean readyForQCBefore = isMaterialsReadyForQC(before);
 
 		if (readyForQCBefore)
 		{
-			return; // asset was already ready for qc before this attribute update, nothing more to do
+			log.debug("asset was already ready for qc before this attribute update.");
+			return; 
 		}
 
 		boolean readyForQCAfter = isMaterialsReadyForQC(currentAttributes);
 
 		if (!readyForQCAfter)
 		{
-			return; // not ready for qc after the attribute update, nothing more to do.
+			log.debug("asset is not ready for QC yet, continue to wait for asset update.");
+			return; 
 		}
 
-		if ((!readyForQCBefore) && readyForQCAfter) // ought to always be true if we have got this far but put in anyway to made the conditions explicit
+		if ((!readyForQCBefore) && readyForQCAfter)
 		{
 			log.info(String.format("Asset %s (%s) was not ready for qc and now is", houseID, assetID));
 
-			// finish any ingest tasks or other on ingest logic
+			try
+            {
+                AttributeMap task = taskController.getOnlyOpenTaskForAssetByAssetID(MayamTaskListType.INGEST, assetID);
+                if (task != null)
+                {
+                	TaskState taskState = (TaskState) task.getAttribute(Attribute.TASK_STATE);
+					if ((null != taskState) && (!taskState.equals(TaskState.FINISHED)))
+					{
+						log.info(String.format("Ingest task is set to finished status for asset %s", assetID));
+                        task.setAttribute(Attribute.TASK_STATE, TaskState.FINISHED);
+                        taskController.saveTask(task);
+					}
+                        
+                }
+                else
+                {
+                        log.debug("Could not find Ingest task for asset " + assetID);
+                }
+            }
+            catch (MayamClientException e)
+            {
+                    log.error("Exception thrown while trying to get Ingest Task for asset " + assetID, e);
+            }
 
 		}
 		else
@@ -84,74 +103,80 @@ public class IngestCompleteHandler extends UpdateAttributeHandler
 
 	private boolean isMaterialsReadyForQC(AttributeMap attributes)
 	{
+		MediaStatus hr = attributes.getAttribute(Attribute.MEDST_HR);
+        MediaStatus lr = attributes.getAttribute(Attribute.MEDST_LR);
 
-		{
-			MediaStatus lr = attributes.getAttribute(Attribute.MEDST_LR);
+        log.debug("MEDST_HR : " + hr);
 
-			log.debug("MEDST_LR : " + lr);
+        if (hr == null)
+        {
+            log.debug("MEDST_HR is null, material is not ready for qc");
+            return false;
+        }
+        else
+        {
+            log.debug("MEDST_HR is " + hr.toString());
+            if (!hr.equals(MediaStatus.READY))
+            {
+                    log.debug("MEDST_HR is not READY, material is not ready for qc");
+                    return false;
+            }
+            else
+            {
+                    log.debug("MEDST_HR is READY");
+            }
+        }
 
-			if (lr == null)
-			{
-				log.debug("MEDST_LR is null, material is not ready for qc");
-				return false;
-			}
-			else if (!lr.equals(MediaStatus.READY))
-			{
-				log.debug("MEDST_LR is not READY, material is not ready for qc");
-				return false;
-			}
+        log.debug("MEDST_LR : " + lr);
 
-		}
+        if (lr == null)
+        {
+            log.debug("MEDST_LR is null, material is not ready for qc");
+            return false;
+        }
+        else
+        {
+            log.debug("MEDST_LR is " + lr.toString());
+            if (!lr.equals(MediaStatus.READY))
+            {
+                    log.debug("MEDST_LR is not READY, material is not ready for qc");
+                    return false;
+            }
+            else
+            {
+                    log.debug("MEDST_LR is READY, material is ready for qc");
+            }
 
-		{
-			MediaStatus hr = attributes.getAttribute(Attribute.MEDST_HR);
+        }
 
-			log.debug("MEDST_HR : " + hr);
+        String assetID = attributes.getAttribute(Attribute.ASSET_ID);
 
-			if (hr == null)
-			{
-				log.debug("MEDST_HR is null, material is not ready for qc");
-				return false;
-			}
-			else if (!hr.equals(MediaStatus.READY))
-			{
-				log.debug("MEDST_HR is not READY, material is not ready for qc");
-				return false;
-			}
-		}
-
-		{
-			String assetID = attributes.getAttribute(Attribute.ASSET_ID);
-
-			try
-			{
-				// get file location, do not accept paths not on hires (preferred locations)
-				String assetPath = materialController.getAssetPath(assetID, false);
-				log.debug(String.format("asset is at %s material is ready for qc", assetPath));
-				return true;
-			}
-			catch (MayamClientException e)
-			{
-				if (e.getErrorcode().equals(MayamClientErrorCode.FILE_NOT_IN_PREFERRED_LOCATION))
-				{
-					log.debug("Asset is not in preferred location (hires), not ready for qc");
-					return false;
-				}
-				else
-				{
-					log.error("unexpected exception queurying assets location", e);
-					return false;
-				}
-			}
-
-		}
+        try
+        {
+            String assetPath = materialController.getAssetPath(assetID, false);
+            log.debug(String.format("asset is at %s material is ready for qc", assetPath));
+            return true;
+        }
+        catch (MayamClientException e)
+        {
+            if (e.getErrorcode().equals(MayamClientErrorCode.FILE_NOT_IN_PREFERRED_LOCATION))
+            {
+                    log.debug("Asset is not in preferred location (hires), not ready for qc", e);
+                    return false;
+            }
+            else
+            {
+                    log.error("unexpected exception queurying assets location", e);
+                    return false;
+            }
+        }
 
 	}
 
 	@Override
-	public String getName()
+	public MayamTaskListType getTaskType()
 	{
-		return "Ingest Complete";
+		return MayamTaskListType.INGEST;
 	}
 
 }
