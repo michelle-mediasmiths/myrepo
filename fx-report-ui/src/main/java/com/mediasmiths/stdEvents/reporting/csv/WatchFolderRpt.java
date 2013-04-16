3e2fe9c339afc7e6ff940ba48bde8e76b234f8f3
alisonboal <alison.boal@mediasmiths.com>
2013-04-16 16:42:47 +0100

    adding watch folder storage report

--- fx-report-ui/src/main/java/com/mediasmiths/stdEvents/reporting/csv/WatchFolderRpt.java
+++ fx-report-ui/src/main/java/com/mediasmiths/stdEvents/reporting/csv/WatchFolderRpt.java
@@ -15,9 +15,11 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import com.mediasmiths.foxtel.ip.common.events.FilePickupDetails;
+import com.mediasmiths.foxtel.ip.common.events.report.Watchfolder;
+import com.mediasmiths.std.util.jaxb.JAXBSerialiser;
 import com.mediasmiths.stdEvents.coreEntity.db.entity.EventEntity;
 import com.mediasmiths.stdEvents.events.rest.api.QueryAPI;
-import com.mediasmiths.stdEvents.report.entity.WatchFolder;
 
 public class WatchFolderRpt
 {
@@ -32,20 +34,56 @@ public class WatchFolderRpt
 	
 	public void writeWatchFolder(List<EventEntity> events, Date startDate, Date endDate, String reportName)
 	{
-		List<WatchFolder> watched = getWatchFolderList(events, startDate, endDate);
+		List<Watchfolder> watched = getWatchFolderList(events, startDate, endDate);
 		
+		createCsv(watched, reportName);
+			
+			
+	}
+	
+	private Object unmarshall(EventEntity event)
+	{
+		Object file = null;
+		String payload = event.getPayload();
+		logger.info("Unmarshalling payload: " + payload);
+		try {
+			JAXBSerialiser JAXB_SERIALISER = JAXBSerialiser.getInstance(com.mediasmiths.foxtel.ip.common.events.ObjectFactory.class);
+			file = JAXB_SERIALISER.deserialise(payload);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+	
+	public List<Watchfolder> getWatchFolderList(List<EventEntity> events, Date startDate, Date endDate)
+	{
+		List<Watchfolder> files = new ArrayList<Watchfolder>();
+		for (EventEntity event : events)
+		{
+			Watchfolder file = new Watchfolder();
+			FilePickupDetails details = (FilePickupDetails) unmarshall(event);
+			file.setDateRange(startDate + " - " + endDate);
+			file.setFilename(details.getFilename());
+			file.setDiscovered(new Date(details.getTimeDiscovered()).toString());
+			file.setProcessed(new Date(details.getTimeProcessed()).toString());
+			files.add(file);
+		}
+		
+		return files;
+	}
+	
+	private void createCsv (List<Watchfolder> files, String reportName)
+	{
 		ICsvBeanWriter beanWriter = null;
 		try { 
 			beanWriter = new CsvBeanWriter(new FileWriter(REPORT_LOC + reportName + ".csv"), CsvPreference.STANDARD_PREFERENCE);
-			final String[] header = {"dateRange", "filename", "timeDiscovered", "timeProcessed", "aggregator"};
+			final String[] header = {"dateRange", "filename", "discovered", "processed", "aggregator"};
 			final CellProcessor[] processors = getWatchFolderProcessor();
 			beanWriter.writeHeader(header);
 			
-			WatchFolder average = new WatchFolder("Average Time", null);
-			watched.add(average);
-			
-			for (WatchFolder watch : watched) {
-				beanWriter.write(watch, header, processors);
+			for (Watchfolder file : files) {
+				beanWriter.write(file, header, processors);
 			}
 		}
 		catch (IOException e)
@@ -65,22 +103,6 @@ public class WatchFolderRpt
 				}
 			}
 		}
-	}
-	
-	public List<WatchFolder> getWatchFolderList(List<EventEntity> events, Date startDate, Date endDate)
-	{
-		List<WatchFolder> watched = new ArrayList<WatchFolder>();
-		for (EventEntity event : events)
-		{
-			String payload = event.getPayload();
-			WatchFolder watch = new WatchFolder();
-			
-			//GET FIELDS FOR REPORT TYPE
-			
-			watched.add(watch);
-		}
-		
-		return watched;
 	}
 	
 	public CellProcessor[] getWatchFolderProcessor() 
