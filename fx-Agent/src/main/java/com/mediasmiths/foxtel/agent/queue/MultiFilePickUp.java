2d738890250a33b90be536419aec5fff7fb66896
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-04-17 10:23:15 +0100

    handle incomplete pickups for FilePickup stats event

--- fx-Agent/src/main/java/com/mediasmiths/foxtel/agent/queue/MultiFilePickUp.java
+++ fx-Agent/src/main/java/com/mediasmiths/foxtel/agent/queue/MultiFilePickUp.java
@@ -552,12 +552,14 @@ public class MultiFilePickUp implements IFilePickup
 		for (String ext: pp.suffixes)
 		{
 			File f = pp.getPickUp(ext);
-			FilePickupDetails pd = new FilePickupDetails();
-			pd.setFilename(f.getName());
-			pd.setFilePath(pp.getRootPath());
-			pd.setTimeDiscovered(f.lastModified());
-			pd.setTimeProcessed(System.currentTimeMillis());
-			pickUpStats.getDetails().add(pd);
+			if(f!=null){ //pickup can be incomplete
+				FilePickupDetails pd = new FilePickupDetails();
+				pd.setFilename(f.getName());
+				pd.setFilePath(pp.getRootPath());
+				pd.setTimeDiscovered(f.lastModified());
+				pd.setTimeProcessed(System.currentTimeMillis());
+				pickUpStats.getDetails().add(pd);
+			}
 		}
 
 		if (eventsEnabled) pickUpEventTimer.saveEvent("http://www.foxtel.com.au/ip/infrastructure", "FilePickUp", pickUpStats);
