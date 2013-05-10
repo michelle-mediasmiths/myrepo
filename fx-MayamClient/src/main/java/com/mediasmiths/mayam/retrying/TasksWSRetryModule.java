a238e2e9bdccf863da5e2b43f3c06aa5a4eae895
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-05-10 14:21:00 +0100

    fix injection

--- fx-MayamClient/src/main/java/com/mediasmiths/mayam/retrying/TasksWSRetryModule.java
+++ fx-MayamClient/src/main/java/com/mediasmiths/mayam/retrying/TasksWSRetryModule.java
@@ -1,14 +1,8 @@
 package com.mediasmiths.mayam.retrying;
 
-import java.net.MalformedURLException;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import com.google.inject.matcher.Matchers;
-import com.google.inject.name.Named;
-import com.mayam.wf.ws.client.TasksClient;
-import com.mediasmiths.mayam.guice.MayamClientModule;
 import com.mediasmiths.mayam.veneer.AssetApiVeneer;
 import com.mediasmiths.mayam.veneer.SegmentApiVeneer;
 import com.mediasmiths.mayam.veneer.TaskApiVeneer;
@@ -27,14 +21,6 @@ public class TasksWSRetryModule extends AbstractModule
 		bindInterceptor(Matchers.subclassesOf(AssetApiVeneer.class),  Matchers.annotatedWith(TasksWSRetryable.class), interceptor);
 		bindInterceptor(Matchers.subclassesOf(SegmentApiVeneer.class), Matchers.annotatedWith(TasksWSRetryable.class), interceptor);
 		bindInterceptor(Matchers.subclassesOf(UserApiVeneer.class),  Matchers.annotatedWith(TasksWSRetryable.class), interceptor);
-	}
-
-	@Provides
-	@Singleton
-	@Named(MayamClientModule.SETUP_TASKS_CLIENT)
-	public TasksClient getSetupTasksClient() throws MalformedURLException
-	{
-		return new TasksClient();
 	}
 
 }
