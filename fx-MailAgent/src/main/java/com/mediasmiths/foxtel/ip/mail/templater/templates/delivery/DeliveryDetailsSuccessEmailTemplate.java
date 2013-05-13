5b888219d161d0d086f819ae320d661528cc6f2a
David Gault <david.gault@mediasmiths.com>
2013-05-13 13:55:54 +0100

    New Delivery Details templates

---
+++ fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/delivery/DeliveryDetailsSuccessEmailTemplate.java
@@ -0,0 +1,33 @@
+package com.mediasmiths.foxtel.ip.mail.templater.templates.delivery;
+
+import com.mediasmiths.foxtel.ip.common.email.MailTemplate;
+import com.mediasmiths.foxtel.ip.common.events.DeliveryDetails;
+import com.mediasmiths.foxtel.ip.mail.templater.EmailTemplateGenerator;
+
+public class DeliveryDetailsSuccessEmailTemplate extends MailTemplate implements EmailTemplateGenerator
+{
+
+	@Override
+	public boolean handles(Object obj)
+	{
+
+		return obj instanceof DeliveryDetails;
+	}
+
+	@Override
+	public MailTemplate customiseTemplate(Object obj, String comment)
+	{
+         MailTemplate t = new MailTemplate();
+
+         DeliveryDetails dd = (DeliveryDetails)obj;
+
+		t.setSubject(String.format(getSubject(), dd.getMasterId(), dd.getTitle()));
+		t.setEmailaddresses(getEmailaddresses());
+		t.setBody(String.format(getBody(), dd.getFileLocation()));
+
+		return t;
+	}
+
+
+
+}
