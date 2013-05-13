a4842dee69a29ef2519ac678b1501de904b435e9
David Gault <david.gault@mediasmiths.com>
2013-05-13 11:04:42 +0100

    Ardome Import Failure Template

---
+++ fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/content/ArdomeImportFailureEmailTemplate.java
@@ -0,0 +1,28 @@
+package com.mediasmiths.foxtel.ip.mail.templater.templates.content;
+
+import com.mediasmiths.foxtel.ip.mail.templater.EmailTemplateGenerator;
+import com.mediasmiths.foxtel.ip.common.events.ArdomeImportFailure;
+import com.mediasmiths.foxtel.ip.common.email.MailTemplate;
+
+public class ArdomeFailureEmailTemplate extends MailTemplate implements EmailTemplateGenerator
+{
+
+	@Override
+	public boolean handles(Object obj)
+	{
+		return obj instanceof ArdomeImportFailure;
+	}
+
+	@Override
+	public MailTemplate customiseTemplate(Object obj, String comment)
+	{
+		ArdomeImportFailure aif = (ArdomeImportFailure) obj;
+		MailTemplate t = new MailTemplate();
+
+		t.setSubject(String.format(getSubject(), aif.getFilename(), aif.getJobID()));
+		t.setBody(getBody());
+		t.setEmailaddresses(getEmailaddresses());
+		return t;
+	}
+
+}
