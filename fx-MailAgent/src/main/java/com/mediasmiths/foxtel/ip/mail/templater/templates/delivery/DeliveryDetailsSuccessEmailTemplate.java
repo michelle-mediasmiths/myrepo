ee6bf07b96f11dcd7084c3a6f0148565475b8cfa
David Gault <david.gault@mediasmiths.com>
2013-05-13 15:47:37 +0100

    New Extended Publishing Details type

--- fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/delivery/DeliveryDetailsSuccessEmailTemplate.java
+++ fx-MailAgent/src/main/java/com/mediasmiths/foxtel/ip/mail/templater/templates/delivery/DeliveryDetailsSuccessEmailTemplate.java
@@ -1,7 +1,8 @@
 package com.mediasmiths.foxtel.ip.mail.templater.templates.delivery;
 
 import com.mediasmiths.foxtel.ip.common.email.MailTemplate;
-import com.mediasmiths.foxtel.ip.common.events.DeliveryDetails;
+import com.mediasmiths.foxtel.ip.common.events.ExtendedPublishingDetails;
+import com.mediasmiths.foxtel.ip.mail.templater.EmailListTransform;
 import com.mediasmiths.foxtel.ip.mail.templater.EmailTemplateGenerator;
 
 public class DeliveryDetailsSuccessEmailTemplate extends MailTemplate implements EmailTemplateGenerator
@@ -11,7 +12,7 @@ public class DeliveryDetailsSuccessEmailTemplate extends MailTemplate implements
 	public boolean handles(Object obj)
 	{
 
-		return obj instanceof DeliveryDetails;
+		return obj instanceof ExtendedPublishingDetails;
 	}
 
 	@Override
@@ -19,10 +20,12 @@ public class DeliveryDetailsSuccessEmailTemplate extends MailTemplate implements
 	{
          MailTemplate t = new MailTemplate();
 
-         DeliveryDetails dd = (DeliveryDetails)obj;
+         ExtendedPublishingDetails dd = (ExtendedPublishingDetails)obj;
 
 		t.setSubject(String.format(getSubject(), dd.getMaterialID(), dd.getTitle()));
 		t.setEmailaddresses(getEmailaddresses());
+		t.getEmailaddresses().getEmailaddress().addAll(EmailListTransform.toEmailAddressList(dd.getEmailaddresses().getEmailaddress()));
+
 		t.setBody(String.format(getBody(), dd.getFileLocation()));
 
 		return t;
