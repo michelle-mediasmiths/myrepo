9b3502f972b1f5b9b20b2879c6c02c3b3c87ffa8
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-05-15 12:18:13 +0100

    MAM-550 cant rely on the before and after transitions for pending tx packages, always try to create tx pacakge if it seems 'ready'

--- fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/pendingtx/PendingTxUpdateHandler.java
+++ fx-MqClient/src/main/java/com/mediasmiths/mq/handlers/pendingtx/PendingTxUpdateHandler.java
@@ -2,7 +2,7 @@ package com.mediasmiths.mq.handlers.pendingtx;
 
 import java.util.EnumSet;
 
-import org.apache.hadoop.mapred.TaskController;
+
 import org.apache.log4j.Logger;
 
 import com.mayam.wf.attributes.shared.Attribute;
@@ -11,7 +11,7 @@ import com.mayam.wf.attributes.shared.type.SegmentList;
 import com.mayam.wf.attributes.shared.type.TaskState;
 import com.mediasmiths.mayam.MayamClientException;
 import com.mediasmiths.mayam.MayamTaskListType;
-import com.mediasmiths.mayam.controllers.MayamMaterialController;
+import com.mediasmiths.mayam.PackageNotFoundException;
 import com.mediasmiths.mayam.controllers.MayamPackageController;
 import com.mediasmiths.mayam.util.AssetProperties;
 import com.mediasmiths.mq.handlers.TaskUpdateHandler;
@@ -30,38 +30,54 @@ public class PendingTxUpdateHandler extends TaskUpdateHandler
 		if (OPEN_STATES.contains(taskState))
 		{
 
-			boolean packagesConsideredPendingBefore = AssetProperties.shouldPackagesForMaterialBeConsideredPending(before);	
-			
-			boolean fromAuthSourceBefore =  packageSeenFromAuthoritativeSource(before);
-			
-			boolean packagesConsideredPendingAfter = AssetProperties.shouldPackagesForMaterialBeConsideredPending(currentAttributes);			
-			boolean fromAuthSourceAfter=  packageSeenFromAuthoritativeSource(currentAttributes);
+			boolean packagesConsideredPendingAfter = AssetProperties.shouldPackagesForMaterialBeConsideredPending(currentAttributes);
+			boolean fromAuthSourceAfter = packageSeenFromAuthoritativeSource(currentAttributes);
 
-			boolean shouldPackageBeCreatedBefore = (!packagesConsideredPendingBefore) && fromAuthSourceBefore;
-			boolean shouldPackageBeCreatedAfter = (!packagesConsideredPendingAfter) && fromAuthSourceAfter ;
-			
+			boolean shouldPackageBeCreatedAfter = (!packagesConsideredPendingAfter) && fromAuthSourceAfter;
 
 			log.debug(String.format(
-					"packagesConsideredPendingBefore %b, fromAuthSourceBefore %b, packagesConsideredPendingAfter %b, fromAuthSourceAfter %b, shouldPackageBeCreatedBefore %b, shouldPackageBeCreatedAfter %b",
-					packagesConsideredPendingBefore,
-					fromAuthSourceBefore,
+					"packagesConsideredPendingAfter %b, fromAuthSourceAfter %b, shouldPackageBeCreatedAfter %b",
 					packagesConsideredPendingAfter,
 					fromAuthSourceAfter,
-					shouldPackageBeCreatedBefore,
 					shouldPackageBeCreatedAfter));
-			
-			
-			boolean pendingStateChanges = (!shouldPackageBeCreatedBefore) && shouldPackageBeCreatedAfter; 
-			
-			if (pendingStateChanges)
+
+			if (shouldPackageBeCreatedAfter)
 			{
-				log.info("item is now ready for tx packages to be created (package has been seen coming from bms)");
+				log.info("item is now ready for tx packages to be created (package has been seen coming from bms and item has passed qc)");
 
 				AttributeMap updateMap = taskController.updateMapForTask(currentAttributes);
 
 				SegmentList segmentList = currentAttributes.getAttribute(Attribute.SEGMENTATION_LIST);
-				
-				
+
+				String packageID = (String) currentAttributes.getAttribute(Attribute.AUX_EXTIDSTR);
+
+				if (packageID == null)
+				{
+					errorWithReason(updateMap, "AUX_EXTIDSTR null on a pending tx package task");
+					return;
+				}
+
+				try
+				{
+					log.debug("attemping to load segmentlist with  id " + packageID);
+					SegmentList realPackage = packageController.getSegmentList(packageID);
+
+					if (realPackage != null)
+					{
+						log.error(String.format("Package %s already exists", packageID));
+						errorWithReason(updateMap, "Package already exists");
+						return;
+					}
+					else
+					{
+						log.debug("No real package with id " + packageID);
+					}
+				}
+				catch (PackageNotFoundException e2)
+				{
+					log.debug("ignoring package not found exception for package " + packageID
+							+ " as it is not expected to currently exist");
+				}
 
 				try
 				{
@@ -78,14 +94,14 @@ public class PendingTxUpdateHandler extends TaskUpdateHandler
 				{
 					log.error("error searching for fix and stitch tasks, segmentation information will be populated", e);
 				}
-				
+
 				try
 				{
 					packageController.createSegmentList(
 							currentAttributes.getAttributeAsString(Attribute.AUX_EXTIDSTR),
 							currentAttributes.getAttributeAsString(Attribute.ASSET_ID),
 							segmentList);
-					
+
 					try
 					{
 						// update the pending tx delivery task
@@ -96,24 +112,30 @@ public class PendingTxUpdateHandler extends TaskUpdateHandler
 					{
 						log.error("error updating pending tx delivery task status", e);
 					}
-					
+
 				}
 				catch (MayamClientException e)
 				{
 					log.error("error creating segment list", e);
-					updateMap.setAttribute(Attribute.TASK_STATE, TaskState.ERROR);
-					updateMap.setAttribute(Attribute.ERROR_MSG, "Error creating segmentlist "+e.getErrorcode().toString());
-					try
-					{
-						taskController.saveTask(updateMap);
-					}
-					catch (MayamClientException e1)
-					{
-						log.error("error updating pending tx package task to error state", e1);
-					}
+					errorWithReason(updateMap, "Error creating segmentlist " + e.getErrorcode().toString());
 				}
 
 			}
+		}
+	}
+
+	private void errorWithReason(AttributeMap updateMap, String reason)
+	{
+
+		updateMap.setAttribute(Attribute.TASK_STATE, TaskState.ERROR);
+		updateMap.setAttribute(Attribute.ERROR_MSG, reason);
+		try
+		{
+			taskController.saveTask(updateMap);
+		}
+		catch (MayamClientException e1)
+		{
+			log.error("error updating pending tx package task to error state", e1);
 		}
 	}
 
@@ -123,13 +145,14 @@ public class PendingTxUpdateHandler extends TaskUpdateHandler
 	private boolean packageSeenFromAuthoritativeSource(AttributeMap currentAttributes)
 	{
 		String auxSrc = currentAttributes.getAttribute(Attribute.AUX_SRC);
-		
-		log.info("AUX_SRC is"+auxSrc);
-		
-		if(auxSrc!=null && auxSrc.equals(MayamPackageController.PENDING_TX_PACKAGE_SOURCE_BMS)){
+
+		log.info("AUX_SRC is" + auxSrc);
+
+		if (auxSrc != null && auxSrc.equals(MayamPackageController.PENDING_TX_PACKAGE_SOURCE_BMS))
+		{
 			return true;
 		}
-		
+
 		return false;
 	}
 
