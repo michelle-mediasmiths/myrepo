52b09281899915e1c712cc73b16d84081818932f
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-05-21 13:09:45 +0100

    add writecompanion end point to wfadapter for export tasks metadata write

---
+++ fx-WFAdapterAPI/src/main/java/com/mediasmiths/foxtel/wf/adapter/model/WriteExportCompanions.java
@@ -0,0 +1,21 @@
+package com.mediasmiths.foxtel.wf.adapter.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class WriteExportCompanions
+{
+	private Long taskID;
+
+	@XmlElement(required=true)
+	public Long getTaskID()
+	{
+		return taskID;
+	}
+
+	public void setTaskID(Long taskID)
+	{
+		this.taskID = taskID;
+	}
+}
