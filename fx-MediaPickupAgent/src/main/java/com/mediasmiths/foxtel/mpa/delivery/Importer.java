0780136061db1088f30421782b1d97fa08415a6a
Bryce McLeod <bryce.mcleod@mediasmiths.com>
2013-06-07 13:33:19 +0100

    formatting

--- fx-MediaPickupAgent/src/main/java/com/mediasmiths/foxtel/mpa/delivery/Importer.java
+++ fx-MediaPickupAgent/src/main/java/com/mediasmiths/foxtel/mpa/delivery/Importer.java
@@ -1,16 +1,5 @@
 package com.mediasmiths.foxtel.mpa.delivery;
 
-import static com.mediasmiths.foxtel.agent.Config.WATCHFOLDER_LOCATIONS;
-import static com.mediasmiths.foxtel.mpa.MediaPickupConfig.DELIVERY_ATTEMPT_COUNT;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.log4j.Logger;
-
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.mediasmiths.foxtel.agent.WatchFolders;
@@ -26,31 +15,44 @@ import com.mediasmiths.foxtel.ip.event.EventService;
 import com.mediasmiths.foxtel.mpa.MediaEnvelope;
 import com.mediasmiths.foxtel.mpa.PendingImport;
 import com.mediasmiths.std.util.jaxb.JAXBSerialiser;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import static com.mediasmiths.foxtel.agent.Config.WATCHFOLDER_LOCATIONS;
+import static com.mediasmiths.foxtel.mpa.MediaPickupConfig.DELIVERY_ATTEMPT_COUNT;
 
 
-public class Importer {
+public class Importer
+{
 
 	private static Logger logger = Logger.getLogger(Importer.class);
 
 
 	// private final String targetFolder;
 	private final WatchFolders watchFolders; // contains source to destination
-												// mappings for delivery
+	// mappings for delivery
 	private final int deliveryAttemptsToMake;
 
 	private final EventService eventService;
 
 	public static final JAXBSerialiser JAXB_SERIALISER = JAXBSerialiser.getInstance("com.mediasmiths.foxtel.ip.common.events.report");
 
+
 	@Inject
-	public Importer(
-			@Named(WATCHFOLDER_LOCATIONS) WatchFolders watchFolders,
-			@Named(DELIVERY_ATTEMPT_COUNT) String deliveryAttemptsToMake,
-			com.mediasmiths.foxtel.ip.event.EventService eventService) {
+	public Importer(@Named(WATCHFOLDER_LOCATIONS) WatchFolders watchFolders,
+	                @Named(DELIVERY_ATTEMPT_COUNT) String deliveryAttemptsToMake,
+	                com.mediasmiths.foxtel.ip.event.EventService eventService)
+	{
 		this.watchFolders = watchFolders;
 		this.deliveryAttemptsToMake = Integer.parseInt(deliveryAttemptsToMake);
 		this.eventService = eventService;
 	}
+
 
 	/**
 	 * Once the WFE has established the match between the delivered media file
@@ -60,14 +62,15 @@ public class Importer {
 	 * of the Item placeholder in Viz Ardome. Once the media file has been moved
 	 * to the Viz Ardome auto import location, the WFE shall move the companion
 	 * XML file to a corporate share to be nominated by FOXTEL.
-	 * 
+	 *
 	 * @param pi
 	 */
-	public void deliver(PendingImport pi, int attempt) {
+	public void deliver(PendingImport pi, int attempt)
+	{
 
 
-		if (pi.getMaterialEnvelope().getMasterID() == null
-				|| pi.getMaterialEnvelope().getMasterID().equals("null")) {
+		if (pi.getMaterialEnvelope().getMasterID() == null || pi.getMaterialEnvelope().getMasterID().equals("null"))
+		{
 			logger.error("Missing masterID in PendingImport");
 			throw new IllegalArgumentException("masterid/materialid or equivalent required to deliver to ardome import folder");
 		}
@@ -75,31 +78,38 @@ public class Importer {
 		File src = pi.getMaterialEnvelope().getPickupPackage().getPickUp("mxf");
 
 		final long fileSize = src.length();
-		
+
 		String targetFolder = watchFolders.destinationFor(FilenameUtils.getFullPathNoEndSeparator(src.getAbsolutePath()));
-		File dst = new File(targetFolder, pi.getMaterialEnvelope()
-				.getMasterID() + ".mxf");
+		File dst = new File(targetFolder, pi.getMaterialEnvelope().getMasterID() + ".mxf");
 
-		logger.debug(String.format(
-				"Attempting to move from %s to %s (attempt %d)",
-				src.getAbsolutePath(), dst.getAbsolutePath(), attempt));
+		logger.debug(String.format("Attempting to move from %s to %s (attempt %d)",
+		                           src.getAbsolutePath(),
+		                           dst.getAbsolutePath(),
+		                           attempt));
 
-		try {
+		try
+		{
 			FileUtils.moveFile(src, dst);
-		} catch (IOException e) {
-			logger.error(String.format(
-					"Error moving file from %s to %s on attempt number %d",
-					src.getAbsolutePath(), dst.getAbsolutePath(), attempt), e);
-			
+		}
+		catch (IOException e)
+		{
+			logger.error(String.format("Error moving file from %s to %s on attempt number %d",
+			                           src.getAbsolutePath(),
+			                           dst.getAbsolutePath(),
+			                           attempt), e);
+
 			FilePickupDetails fpd = new FilePickupDetails();
 			fpd.setFilename(src.getName());
 			fpd.setTimeProcessed((new Date().getTime()));
 			eventService.saveEvent("http://www.foxtel.com.au/ip/content", "FilePickUpNotification", fpd);
 
 			// allows a configurable number of retries
-			if (attempt == deliveryAttemptsToMake) {
+			if (attempt == deliveryAttemptsToMake)
+			{
 				onDeliveryFailure(pi);
-			} else {
+			}
+			else
+			{
 				deliver(pi, attempt + 1);
 			}
 
@@ -107,47 +117,47 @@ public class Importer {
 		}
 
 		src = pi.getMaterialEnvelope().getPickupPackage().getPickUp("xml");
-		dst = new File(MessageProcessor.getArchivePathForFile(src
-				.getAbsolutePath()), pi.getMaterialEnvelope().getMasterID()
-				+ ".xml");
-		
-		if(dst.exists()){
-			int i=1;
-			
-			while(dst.exists()){
-				dst = new File(MessageProcessor.getArchivePathForFile(src
-						.getAbsolutePath()), pi.getMaterialEnvelope().getMasterID()
-						+ "_"+ i +".xml");
+		dst = new File(MessageProcessor.getArchivePathForFile(src.getAbsolutePath()),
+		               pi.getMaterialEnvelope().getMasterID() + ".xml");
+
+		if (dst.exists())
+		{
+			int i = 1;
+
+			while (dst.exists())
+			{
+				dst = new File(MessageProcessor.getArchivePathForFile(src.getAbsolutePath()),
+				               pi.getMaterialEnvelope().getMasterID() + "_" + i + ".xml");
 				i++;
 			}
 		}
 
-		logger.debug(String.format("Attempting to move from %s to %s",
-				src.getAbsolutePath(), dst.getAbsolutePath()));
+		logger.debug(String.format("Attempting to move from %s to %s", src.getAbsolutePath(), dst.getAbsolutePath()));
 
-		try {
+		try
+		{
 			FileUtils.moveFile(src, dst);
-		} catch (IOException e) {
-			logger.error(
-					String.format(
-							"Error moving file from %s to %s though move for media succeeded",
-							src.getAbsolutePath(), dst.getAbsolutePath()), e);
+		}
+		catch (IOException e)
+		{
+			logger.error(String.format("Error moving file from %s to %s though move for media succeeded",
+			                           src.getAbsolutePath(),
+			                           dst.getAbsolutePath()), e);
 
 			// send out alert that companion xml did not move to archive
 			StringBuilder sb = new StringBuilder();
-			sb.append(String
-					.format("There has been a failure to archive companion xml for material %s though the material successfully moved to the Viz Ardome auto import location",
-							pi.getMaterialEnvelope().getMasterID()));
+			sb.append(String.format("There has been a failure to archive companion xml for material %s though the material successfully moved to the Viz Ardome auto import location",
+			                        pi.getMaterialEnvelope().getMasterID()));
 			eventService.saveEvent("error", sb.toString());
-		
+
 			MediaPickupNotification mpn = new MediaPickupNotification();
 			mpn.setFilelocation(src.getAbsolutePath());
 			mpn.setTime((new Date()).toString());
 			eventService.saveEvent("http://www.foxtel.com.au/ip/content", "ContentWithoutCompanionXML", mpn);
-			
+
 			return;
 		}
-		saveEvent(pi,fileSize);
+		saveEvent(pi, fileSize);
 	}
 
 
@@ -157,51 +167,49 @@ public class Importer {
 		{
 			Acquisition payload = new Acquisition();
 
-			@SuppressWarnings("rawtypes")
-			MediaEnvelope materialEnvelope = pi.getMaterialEnvelope();
+			@SuppressWarnings("rawtypes") MediaEnvelope materialEnvelope = pi.getMaterialEnvelope();
 			Object message = materialEnvelope.getMessage();
 
-			if(message instanceof Material){
+			if (message instanceof Material)
+			{
 
 				Material m = (Material) message;
 
-				if(com.mediasmiths.foxtel.mpa.Util.isProgramme(m))
-					{
-						ProgrammeMaterialType p = m.getTitle().getProgrammeMaterial();
+				if (com.mediasmiths.foxtel.mpa.Util.isProgramme(m))
+				{
+					ProgrammeMaterialType p = m.getTitle().getProgrammeMaterial();
 
-						payload.setMaterialID(p.getMaterialID());
-						payload.setTitle(m.getTitle().getEpisodeTitle());
-						payload.setAggregatorID(m.getDetails().getSupplier().getSupplierID());
-						payload.setFormat(p.getFormat());
-						payload.setFileDelivery(true);
-						payload.setTapeDelivery(false);
-						payload.setFilesize(fileSize+"");
-						payload.setTitleLength(p.getDuration());
+					payload.setMaterialID(p.getMaterialID());
+					payload.setTitle(m.getTitle().getEpisodeTitle());
+					payload.setAggregatorID(m.getDetails().getSupplier().getSupplierID());
+					payload.setFormat(p.getFormat());
+					payload.setFileDelivery(true);
+					payload.setTapeDelivery(false);
+					payload.setFilesize(fileSize + "");
+					payload.setTitleLength(p.getDuration());
 
-						eventService.saveEvent("ProgrammeContentAvailable", JAXB_SERIALISER.serialise(payload));
-					}
-					else
-					{
-						MarketingMaterialType marketingMaterial = m.getTitle().getMarketingMaterial();
+					eventService.saveEvent("ProgrammeContentAvailable", JAXB_SERIALISER.serialise(payload));
+				}
+				else
+				{
+					MarketingMaterialType marketingMaterial = m.getTitle().getMarketingMaterial();
 
-						payload.setMaterialID(materialEnvelope.getMasterID());
-						payload.setTitle(m.getTitle().getEpisodeTitle());
-						payload.setAggregatorID(m.getDetails().getSupplier().getSupplierID());
-						payload.setFormat(marketingMaterial.getFormat());
-						payload.setTapeDelivery(false);
-						payload.setFileDelivery(true);
-						payload.setFilesize(fileSize+"");
-						payload.setTitleLength(marketingMaterial.getDuration());
+					payload.setMaterialID(materialEnvelope.getMasterID());
+					payload.setTitle(m.getTitle().getEpisodeTitle());
+					payload.setAggregatorID(m.getDetails().getSupplier().getSupplierID());
+					payload.setFormat(marketingMaterial.getFormat());
+					payload.setTapeDelivery(false);
+					payload.setFileDelivery(true);
+					payload.setFilesize(fileSize + "");
+					payload.setTitleLength(marketingMaterial.getDuration());
 
 
-						eventService.saveEvent("MarketingContentAvailable", JAXB_SERIALISER.serialise(payload));
-
-					}
-
+					eventService.saveEvent("MarketingContentAvailable", JAXB_SERIALISER.serialise(payload));
+				}
 			}
-			else if(message instanceof RuzzIngestRecord)
+			else if (message instanceof RuzzIngestRecord)
 			{
-				RuzzIngestRecord r = (RuzzIngestRecord)message;
+				RuzzIngestRecord r = (RuzzIngestRecord) message;
 
 				payload.setMaterialID(r.getMaterial().getMaterialID());
 				payload.setTitle(r.getMaterial().getDetails().getTitle());
@@ -209,11 +217,10 @@ public class Importer {
 				payload.setFormat(r.getMaterial().getDetails().getFormat());
 				payload.setTapeDelivery(false);
 				payload.setFileDelivery(true);
-				payload.setFilesize(fileSize+"");
+				payload.setFilesize(fileSize + "");
 				payload.setTitleLength(r.getMaterial().getDetails().getDuration());
 
 				eventService.saveEvent("ProgrammeContentAvailable", JAXB_SERIALISER.serialise(payload));
-
 			}
 			else
 			{
@@ -226,6 +233,7 @@ public class Importer {
 		}
 	}
 
+
 	/**
 	 * 2.1.2.5 Moving media file to either Viz Ardome auto import location fails
 	 * If the WFE fails to move the file to the auto import location after a
@@ -234,33 +242,35 @@ public class Importer {
 	 * send an e-mail notification to an e-mail alias (or list) to be defined by
 	 * FOXTEL
 	 */
-	private void onDeliveryFailure(PendingImport pi) {
-		try {
+	private void onDeliveryFailure(PendingImport pi)
+	{
+		try
+		{
 			File src = pi.getMaterialEnvelope().getPickupPackage().getPickUp("mxf");
-			String failureFolder = MessageProcessor
-					.getFailureFolderForFile(src);
-			File baseDestination = new File(failureFolder, pi
-					.getMaterialEnvelope().getMasterID()
-					+ FilenameUtils.EXTENSION_SEPARATOR + "mxf");
-			File dst = new File(MessageProcessor.getDestinationPathForFileMove(
-					baseDestination, failureFolder, true));
+			String failureFolder = MessageProcessor.getFailureFolderForFile(src);
+			File baseDestination = new File(failureFolder,
+			                                pi.getMaterialEnvelope().getMasterID() + FilenameUtils.EXTENSION_SEPARATOR + "mxf");
+			File dst = new File(MessageProcessor.getDestinationPathForFileMove(baseDestination, failureFolder, true));
 
-			try {
+			try
+			{
 				FileUtils.moveFile(src, dst);
-			} catch (IOException e) {
-				logger.fatal(String.format("Error moving file from %s to %s",
-						src, dst), e);
+			}
+			catch (IOException e)
+			{
+				logger.fatal(String.format("Error moving file from %s to %s", src, dst), e);
 			}
 
 			src = pi.getMaterialEnvelope().getPickupPackage().getPickUp("xml");
-			dst = new File(failureFolder, pi.getMaterialEnvelope()
-					.getMasterID() + FilenameUtils.EXTENSION_SEPARATOR + "xml");
+			dst = new File(failureFolder, pi.getMaterialEnvelope().getMasterID() + FilenameUtils.EXTENSION_SEPARATOR + "xml");
 
-			try {
+			try
+			{
 				FileUtils.moveFile(src, dst);
-			} catch (IOException e) {
-				logger.fatal(String.format("Error moving file from %s to %s",
-						src, dst), e);
+			}
+			catch (IOException e)
+			{
+				logger.fatal(String.format("Error moving file from %s to %s", src, dst), e);
 			}
 
 			try
@@ -272,21 +282,17 @@ public class Importer {
 			}
 			catch (Exception e)
 			{
-                logger.error("Unable to send Quarantine event ", e);
+				logger.error("Unable to send Quarantine event ", e);
 			}
-		} finally {
+		}
+		finally
+		{
 
 			// send out alert for failed import
 			StringBuilder sb = new StringBuilder();
-			sb.append(String
-					.format("There has been a failure to deliver material to the Viz Ardome auto import location"));
-			sb.append(String.format(
-					"The material with ID %s has been quarrentined", pi
-							.getMaterialEnvelope().getMasterID()));
+			sb.append(String.format("There has been a failure to deliver material to the Viz Ardome auto import location"));
+			sb.append(String.format("The material with ID %s has been quarrentined", pi.getMaterialEnvelope().getMasterID()));
 			eventService.saveEvent("error", sb.toString());
-
 		}
-
 	}
-	
 }
