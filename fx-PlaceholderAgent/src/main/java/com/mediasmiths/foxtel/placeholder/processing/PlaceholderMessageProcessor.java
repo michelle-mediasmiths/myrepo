770f61e8d83aae3728dca7feb10333c3a28aebaa
David Gault <david.gault@mediasmiths.com>
2013-06-10 12:18:18 +0100

    MAM-563: Using ASSET_TITLE attribute

--- fx-PlaceholderAgent/src/main/java/com/mediasmiths/foxtel/placeholder/processing/PlaceholderMessageProcessor.java
+++ fx-PlaceholderAgent/src/main/java/com/mediasmiths/foxtel/placeholder/processing/PlaceholderMessageProcessor.java
@@ -13,6 +13,7 @@ import au.com.foxtel.cf.mam.pms.PurgeTitle;
 import au.com.foxtel.cf.mam.pms.RightsType;
 import au.com.foxtel.cf.mam.pms.Source;
 import com.google.inject.Inject;
+import com.mayam.wf.attributes.shared.Attribute;
 import com.mayam.wf.attributes.shared.AttributeMap;
 import com.mediasmiths.foxtel.agent.MessageEnvelope;
 import com.mediasmiths.foxtel.agent.ReceiptWriter;
@@ -510,7 +511,7 @@ public class PlaceholderMessageProcessor extends MessageProcessor<PlaceholderMes
 					AttributeMap title = mayamClient.getTitle(titleID);
 					if (title != null)
 					{
-						ppf.setTitle(title);
+						ppf.setTitle(title.getAttributeAsString(Attribute.ASSET_TITLE));
 					}
 					Set<String> channelsForTitle = mayamClient.getChannelGroupsForTitle(titleID);
 					ppf.getChannelGroup().addAll(channelsForTitle);
